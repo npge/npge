dedc86e74eff3af34e06330ada5d29f6d57ca075
Boris Nagaev <bnagaev@gmail.com>
2012-05-03 16:10:20 +0400

    add test BloomFilter_default_constructor

--- src/test/bloom-filter.cpp
+++ src/test/bloom-filter.cpp
@@ -21,3 +21,17 @@ BOOST_AUTO_TEST_CASE (BloomFilter_test) {
     BOOST_CHECK(!filter.test("ttgc"));
 }
 
+BOOST_AUTO_TEST_CASE (BloomFilter_default_constructor) {
+    br::BloomFilter filter;
+    filter.set_bits(50);
+    filter.set_hashes(5);
+    BOOST_REQUIRE(filter.bits() == 50);
+    BOOST_REQUIRE(filter.hashes() == 5);
+    filter.add("atgc");
+    filter.add("aaaa");
+    BOOST_REQUIRE(filter.test("atgc"));
+    BOOST_REQUIRE(filter.test("aaaa"));
+    BOOST_CHECK(!filter.test("gggg"));
+    BOOST_CHECK(!filter.test("ttgc"));
+}
+
