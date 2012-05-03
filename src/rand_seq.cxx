48b9c9006971ed4fc6b5ad1cbca7747d7729fdf6
Boris Nagaev <bnagaev@gmail.com>
2012-05-03 23:33:45 +0400

    rand_seq: accept sequence length

--- src/rand_seq.cxx
+++ src/rand_seq.cxx
@@ -8,12 +8,18 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <boost/lexical_cast.hpp>
 
-int main() {
+int main(int argc, char** argv) {
+    int length = 1e6;
+    if (argc == 2) {
+        length = boost::lexical_cast<int>(argv[1]);
+    }
+    const int LINE = 60;
     std::srand(std::time(NULL));
     std::cout << ">test" << std::endl;
-    for (int line = 0; line < 100000; line++) {
-        for (int i = 0; i < 60; i++) {
+    for (int line = 0; line < length / LINE + 1; line++) {
+        for (int i = 0; i < LINE && line * LINE + i < length; i++) {
             std::cout << ("atgc"[std::rand() % 4]);
         }
         std::cout << std::endl;
