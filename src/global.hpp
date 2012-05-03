651ffc7f37616c90a314fe1a86d9bd0afabe66cd
Boris Nagaev <bnagaev@gmail.com>
2012-05-03 17:32:24 +0400

    add file global.hpp with class declaration

---
+++ src/global.hpp
@@ -0,0 +1,18 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2012 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef BR_GLOBAL_HPP_
+#define BR_GLOBAL_HPP_
+
+namespace bloomrepeats {
+
+class BloomFilter;
+
+}
+
+#endif
+
