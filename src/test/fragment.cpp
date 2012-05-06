d156566a17cefb65b8761e3d29bc44f852630712
Boris Nagaev <bnagaev@gmail.com>
2012-05-06 11:45:14 +0400

    add test case for Fragment

---
+++ src/test/fragment.cpp
@@ -0,0 +1,27 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2012 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <boost/test/unit_test.hpp>
+#include <boost/make_shared.hpp>
+
+#include "Sequence.hpp"
+#include "Fragment.hpp"
+
+BOOST_AUTO_TEST_CASE (Fragment_main) {
+    using namespace bloomrepeats;
+    SequencePtr s1 = boost::make_shared<InMemorySequence>("tggtccgagatgcgggcc");
+    Fragment f1(s1, 0, 9, 1);
+    BOOST_REQUIRE(f1.length() == 10);
+    BOOST_REQUIRE(f1.str() == "tggtccgaga");
+    BOOST_REQUIRE(*f1.begin() == 't');
+    Fragment f2(s1, 0, 9, -1);
+    BOOST_REQUIRE(f2.length() == 10);
+    BOOST_REQUIRE(f2.str() == "tctcggacca");
+    BOOST_REQUIRE(*f2.begin() == 'a');
+    BOOST_REQUIRE(f2.begin() - f2.end() == 10);
+}
+
