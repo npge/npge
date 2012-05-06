bc911530dba333183571c02f887cc8353e0901e4
Boris Nagaev <bnagaev@gmail.com>
2012-05-06 16:35:40 +0400

    add test for Sequnce::next_fragment()

--- src/test/sequence.cpp
+++ src/test/sequence.cpp
@@ -8,9 +8,20 @@
 #include <boost/test/unit_test.hpp>
 
 #include "Sequence.hpp"
+#include "Fragment.hpp"
 
 BOOST_AUTO_TEST_CASE (Sequence_main) {
-    bloomrepeats::InMemorySequence seq("tggtccgagatgcgggcccgtaagcttacatacagg");
+    using namespace bloomrepeats;
+    InMemorySequence seq("tggtccgagatgcgggcccgtaagcttacatacagg");
     BOOST_REQUIRE(seq.approximate_size() == 36);
+    SequencePtr s1 = boost::make_shared<InMemorySequence>("tgg");
+    BOOST_REQUIRE(s1->approximate_size() == 3);
+    size_t fragments_number = 0;
+    Fragment f(s1);
+    s1->make_first_fragment(f, 2);
+    while (s1->next_fragment(f)) {
+        fragments_number += 1;
+    }
+    BOOST_REQUIRE(fragments_number == 4);
 }
 
