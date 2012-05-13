cfee420f77c1123b83ebf060a3b6b351c0cfc322
Boris Nagaev <bnagaev@gmail.com>
2012-05-13 22:10:00 +0400

    add include file for boost.program_options

---
+++ src/po.hpp
@@ -0,0 +1,18 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2012 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef BR_PO_HPP_
+#define BR_PO_HPP_
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/value_semantic.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/positional_options.hpp>
+
+#endif
+
