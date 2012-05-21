561b733baeaff8ef101a1ca8103656ad7e1b9b06
Boris Nagaev <bnagaev@gmail.com>
2012-05-21 14:16:09 +0400

    make_hash: add default value for ori = 1

--- src/make_hash.hpp
+++ src/make_hash.hpp
@@ -17,7 +17,7 @@ namespace bloomrepeats {
 \param length Length of the fragment
 \param ori Orientation of the fragment (1 or -1)
 */
-size_t make_hash(const char* start, size_t length, int ori);
+size_t make_hash(const char* start, size_t length, int ori = 1);
 
 }
 
