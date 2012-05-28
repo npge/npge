27de7812f6080f7c724aa84b89b3af2a31af5e11
Boris Nagaev <bnagaev@gmail.com>
2012-05-28 18:18:51 +0400

    add test Fragment_dtor

--- src/test/fragment.cpp
+++ src/test/fragment.cpp
@@ -178,6 +178,33 @@ BOOST_AUTO_TEST_CASE (Fragment_next) {
     BOOST_CHECK(!f3->next());
 }
 
+BOOST_AUTO_TEST_CASE (Fragment_dtor) {
+    using namespace bloomrepeats;
+    SequencePtr s1 = boost::make_shared<InMemorySequence>("tggtcCGAGATgcgggcc");
+    FragmentPtr f1 = boost::make_shared<Fragment>(s1, 1, 2, 1);
+    FragmentPtr f2 = boost::make_shared<Fragment>(s1, 5, 6, -1);
+    FragmentPtr f3 = boost::make_shared<Fragment>(s1, 7, 8, 1);
+    Fragment::connect(f1, f2);
+    Fragment::connect(f2, f3);
+    Fragment::connect(f3, f1);
+    BOOST_REQUIRE(f1->next() == f2);
+    BOOST_REQUIRE(f2->next() == f3);
+    BOOST_REQUIRE(f3->next() == f1);
+    BOOST_REQUIRE(f1->prev() == f3);
+    BOOST_REQUIRE(f2->prev() == f1);
+    BOOST_REQUIRE(f3->prev() == f2);
+    f2.reset(); // (= disconnect(true) )
+    BOOST_CHECK(f1->next() == f3);
+    BOOST_CHECK(f1->prev() == f3);
+    BOOST_CHECK(f3->prev() == f1);
+    BOOST_CHECK(f3->next() == f1);
+    f1->disconnect(/* connect_neighbours */ false);
+    BOOST_CHECK(!f1->prev());
+    BOOST_CHECK(!f1->next());
+    BOOST_CHECK(!f3->prev());
+    BOOST_CHECK(!f3->next());
+}
+
 BOOST_AUTO_TEST_CASE (Fragment_connect_ori) {
     using namespace bloomrepeats;
     SequencePtr s1 = boost::make_shared<InMemorySequence>("tggtcCGAGATgcgggcc");
