a1f2b69e9876a444ca0dbc39e693d4fa4ed54bbc
Boris Nagaev <bnagaev@gmail.com>
2012-06-17 14:14:02 +0400

    add utility program 'rest'

---
+++ src/rest.cxx
@@ -0,0 +1,38 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2012 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "Sequence.hpp"
+#include "BlockSet.hpp"
+
+using namespace bloomrepeats;
+
+int main(int argc, char** argv) {
+    BOOST_ASSERT(argc >= 3);
+    std::string sequences_filename = argv[1];
+    std::string blocks_filename = argv[2];
+    std::ifstream input_file(sequences_filename.c_str());
+    std::vector<SequencePtr> seqs;
+    while (true) {
+        SequencePtr seq(new InMemorySequence(input_file));
+        if (seq->size() > 0) {
+            seqs.push_back(seq);
+        } else {
+            break;
+        }
+    }
+    std::ifstream blocks_file(blocks_filename.c_str());
+    BlockSetPtr block_set = boost::make_shared<BlockSet>();
+    block_set->_read(blocks_file, seqs);
+    block_set->connect_fragments();
+    BlockSetPtr rest = block_set->rest();
+    std::cout << *rest << std::endl;
+}
+
