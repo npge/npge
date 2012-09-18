acac803bbc78474a7c18bbbe77246961b56930d4
Boris Nagaev <bnagaev@gmail.com>
2012-09-18 13:34:57 +0400

    alignment test: add case with gaps

--- src/test/alignment.cpp
+++ src/test/alignment.cpp
@@ -29,3 +29,20 @@ BOOST_AUTO_TEST_CASE (Alignment_main) {
     BOOST_CHECK(a.size() == 0);
 }
 
+BOOST_AUTO_TEST_CASE (Alignment_2) {
+    using namespace bloomrepeats;
+    SequencePtr s1 = boost::make_shared<InMemorySequence>("tggtccgagatgcgggcc");
+    // ----------------------------------------------------012345678901234567
+    Fragment f1(s1, 0, 9, 1);
+    Alignment a;
+    BOOST_REQUIRE(a.add_fragment(&f1, "--tggt---ccgag-a--") == 0);
+    // --------------------------------012345678901234567
+    BOOST_CHECK(a.length() == 18);
+    BOOST_CHECK(a.map_to_alignment(0, 0) == 2);
+    BOOST_CHECK(a.map_to_fragment(0, 2) == 0);
+    BOOST_CHECK(a.map_to_alignment(0, 8) == 13);
+    BOOST_CHECK(a.map_to_fragment(0, 13) == 8);
+    BOOST_CHECK(a.nearest_in_fragment(0, 0) == 0);
+    BOOST_CHECK(a.nearest_in_fragment(0, 6) == 3);
+}
+
