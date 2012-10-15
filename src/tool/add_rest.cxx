0b3bc8570605d427345230a2de13d59d767ada9a
Boris Nagaev <bnagaev@gmail.com>
2012-10-16 00:23:52 +0400

    rewrite add_rest.cxx using process() function

--- src/tool/add_rest.cxx
+++ src/tool/add_rest.cxx
@@ -5,77 +5,40 @@
  * See the LICENSE file for terms of use.
  */
 
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <boost/foreach.hpp>
-
-#include "Sequence.hpp"
-#include "AddBlocks.hpp"
-#include "Block.hpp"
-#include "Fragment.hpp"
+#include "process.hpp"
 #include "BlockSet.hpp"
+#include "Pipe.hpp"
+#include "AddSequences.hpp"
+#include "AddBlocks.hpp"
 #include "Connector.hpp"
-#include "Union.hpp"
-#include "UniqueNames.hpp"
-#include "OverlapsResolver.hpp"
 #include "Rest.hpp"
+#include "Union.hpp"
+#include "Swap.hpp"
+#include "UniqueNames.hpp"
+#include "CheckNoOverlaps.hpp"
+#include "Output.hpp"
 
 using namespace bloomrepeats;
 
+class RestPipe : public Pipe {
+public:
+    RestPipe() {
+        set_block_set(boost::make_shared<BlockSet>());
+        add(new AddSequences);
+        add(new AddBlocks);
+        add(new Connector);
+        BlockSetPtr pre_rest = boost::make_shared<BlockSet>();
+        add(new Swap(pre_rest));
+        add(new Rest(pre_rest));
+        add(new Union(pre_rest));
+        add(new CheckNoOverlaps);
+        add(new UniqueNames);
+        add(new Output);
+    }
+};
+
 int main(int argc, char** argv) {
-    BOOST_ASSERT(argc >= 3);
-    std::string sequences_filename = argv[1];
-    std::string blocks_filename = argv[2];
-    std::ifstream input_file(sequences_filename.c_str());
-    BlockSetPtr block_set = boost::make_shared<BlockSet>();
-#ifndef NDEBUG
-    size_t seq_size_summ = 0;
-#endif
-    while (true) {
-        SequencePtr seq(new InMemorySequence(input_file));
-#ifndef NDEBUG
-        seq_size_summ += seq->size();
-#endif
-        if (seq->size() > 0) {
-            block_set->add_sequence(seq);
-        } else {
-            break;
-        }
-    }
-    AddBlocks::Files files;
-    files.push_back(blocks_filename);
-    AddBlocks blocks_adder;
-    blocks_adder.set_files(files);
-    blocks_adder.apply(block_set);
-    Connector connector;
-    connector.apply(block_set);
-    BlockSetPtr all = boost::make_shared<BlockSet>();
-    Rest r(block_set);
-    r.apply(all);
-#ifndef NDEBUG
-    OverlapsResolver resolver;
-    resolver.set_block_set(all);
-    BOOST_ASSERT(!resolver.overlaps());
-    connector.apply(all);
-    BOOST_ASSERT(!resolver.overlaps());
-#endif
-    Union u(block_set);
-    u.apply(all);
-#ifndef NDEBUG
-    BOOST_ASSERT(!resolver.overlaps());
-    connector.apply(all);
-    BOOST_ASSERT(!resolver.overlaps());
-    size_t fr_size_summ = 0;
-    BOOST_FOREACH (Block* block, *all) {
-        BOOST_FOREACH (Fragment* f, *block) {
-            fr_size_summ += f->length();
-        }
-    }
-    BOOST_ASSERT(fr_size_summ == seq_size_summ);
-#endif
-    UniqueNames names;
-    names.apply(all);
-    std::cout << *all << std::endl;
+    return process(argc, argv, new RestPipe,
+                   "Add unique fragments to current block set");
 }
 
