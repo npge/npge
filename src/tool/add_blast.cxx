7d85d4dc84c5fa3e0ceb23e64a15b55523b68319
Boris Nagaev <bnagaev@gmail.com>
2012-12-10 14:39:27 +0400

    add too add_blast

---
+++ src/tool/add_blast.cxx
@@ -0,0 +1,38 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2012 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include "process.hpp"
+#include "Pipe.hpp"
+#include "AddSequences.hpp"
+#include "AddBlocks.hpp"
+#include "AddBlastBlocks.hpp"
+#include "CleanUp.hpp"
+#include "UniqueNames.hpp"
+#include "CheckNoOverlaps.hpp"
+#include "Output.hpp"
+
+using namespace bloomrepeats;
+
+class AddBlastPipe : public Pipe {
+public:
+    AddBlastPipe() {
+        set_empty_block_set();
+        add(new AddSequences);
+        add(new AddBlocks);
+        add(new AddBlastBlocks);
+        add(new CleanUp);
+        add(new CheckNoOverlaps);
+        add(new UniqueNames);
+        add(new Output);
+    }
+};
+
+int main(int argc, char** argv) {
+    return process(argc, argv, new AddBlastPipe,
+                   "Add blocks found by blast");
+}
+
