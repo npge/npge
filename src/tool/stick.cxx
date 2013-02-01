28e9b33114e18a64dd3dcc826cec167c7c1165ab
Boris Nagaev <bnagaev@gmail.com>
2013-02-01 16:15:25 +0400

    add tool stick (pipe for StickBoundaries)

---
+++ src/tool/stick.cxx
@@ -0,0 +1,33 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2012 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include "process.hpp"
+#include "BlockSet.hpp"
+#include "Pipe.hpp"
+#include "AddSequences.hpp"
+#include "AddBlocks.hpp"
+#include "StickBoundaries.hpp"
+#include "Output.hpp"
+
+using namespace bloomrepeats;
+
+class StickBoundariesPipe : public Pipe {
+public:
+    StickBoundariesPipe() {
+        set_empty_block_set();
+        add(new AddSequences);
+        add(new AddBlocks);
+        add(new StickBoundaries);
+        add(new Output);
+    }
+};
+
+int main(int argc, char** argv) {
+    return process(argc, argv, new StickBoundariesPipe,
+                   "Turn nearby fragment boundaries into one");
+}
+
