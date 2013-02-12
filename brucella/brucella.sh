db6ef14862c00a1b51f27e0194a90244409e704a
Boris Nagaev <bnagaev@gmail.com>
2013-02-12 23:48:56 +0400

    brucella: get fasta from net and replace names

--- brucella/brucella.sh
+++ brucella/brucella.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 TARGET=$1
-PATH=$PATH:$2
+PROJECT_SOURCE_DIR=$2
+PROJECT_BINARY_DIR=$3
+
+export PATH=$PATH:$PROJECT_SOURCE_DIR:$PROJECT_BINARY_DIR/src/tool
+
+TABLE=$PROJECT_SOURCE_DIR/brucella/$TARGET.tsv
+
+get_seqs.py --table $TABLE --out 1-$TARGET.fasta
+replace_names.py --table $TABLE --fasta 1-$TARGET.fasta --out 2-$TARGET.fasta
 
 echo "Ok"
