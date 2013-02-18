c0168b60d33d51e8757eaed6b534cfdeb1ce4907
Boris Nagaev <bnagaev@gmail.com>
2013-02-19 00:55:00 +0400

    add tool resolve_overlaps.cxx
    --HG--
    rename : src/tool/make_pangenome.cxx => src/tool/resolve_overlaps.cxx

---
+++ src/tool/resolve_overlaps.cxx
@@ -0,0 +1,38 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2012 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include "process.hpp"
+#include "Pipe.hpp"
+#include "AddSequences.hpp"
+#include "AddBlocks.hpp"
+#include "Connector.hpp"
+#include "OverlapsResolver.hpp"
+#include "CheckNoOverlaps.hpp"
+#include "UniqueNames.hpp"
+#include "Output.hpp"
+
+using namespace bloomrepeats;
+
+class CleanUpPipe : public Pipe {
+public:
+    CleanUpPipe() {
+        set_empty_block_set();
+        add(new AddSequences);
+        add(new AddBlocks);
+        add(new Connector);
+        add(new OverlapsResolver);
+        add(new CheckNoOverlaps);
+        add(new UniqueNames);
+        add(new Output);
+    }
+};
+
+int main(int argc, char** argv) {
+    return process(argc, argv, new CleanUpPipe,
+                   "Resolve overlaps and expand blocks");
+}
+
