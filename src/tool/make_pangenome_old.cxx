f0f723b481cd0d78e7a61ef8f681c5cf54ce6f95
Boris Nagaev <bnagaev@gmail.com>
2013-02-19 18:44:47 +0400

    revert old overlaps resolver in make_pangenome_old.cxx
    --HG--
    rename : src/tool/make_pangenome.cxx => src/tool/make_pangenome_old.cxx

---
+++ src/tool/make_pangenome_old.cxx
@@ -0,0 +1,68 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2012 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include "process.hpp"
+#include "Pipe.hpp"
+#include "AddSequences.hpp"
+#include "AddBlocks.hpp"
+#include "Joiner.hpp"
+#include "Connector.hpp"
+#include "OverlapsResolver.hpp"
+#include "FragmentsExpander.hpp"
+#include "BlocksExpander.hpp"
+#include "Filter.hpp"
+#include "UniqueNames.hpp"
+#include "CheckNoOverlaps.hpp"
+#include "Output.hpp"
+
+using namespace bloomrepeats;
+
+class CleanUpOld : public Pipe {
+public:
+    CleanUpOld() {
+        using namespace boost;
+        shared_ptr<Filter> filter = make_shared<Filter>();
+        filter->set_min_fragment_length(10);
+        filter->set_no_options(true);
+        add(filter);
+        add(new Connector);
+        add(filter);
+        shared_ptr<OverlapsResolver> resolver = make_shared<OverlapsResolver>();
+        add(resolver);
+        shared_ptr<Joiner> joiner = make_shared<Joiner>(0);
+        joiner->set_no_options(true);
+        add(joiner);
+        add(filter);
+        add(new BlocksExpander);
+        add(resolver);
+        add(new FragmentsExpander);
+        add(new Filter);
+        add(new Joiner(/*max_dist*/ 1000,
+                                    /*ratio_to_fragment*/ 10,
+                                    /*gap_ratio*/ 2));
+    }
+};
+
+class CleanUpPipe : public Pipe {
+public:
+    CleanUpPipe() {
+        set_empty_block_set();
+        add(new AddSequences);
+        add(new AddBlocks);
+        add(new Connector);
+        add(new CleanUpOld);
+        add(new CheckNoOverlaps);
+        add(new UniqueNames);
+        add(new Output);
+    }
+};
+
+int main(int argc, char** argv) {
+    return process(argc, argv, new CleanUpPipe,
+                   "Resolve overlaps and expand blocks");
+}
+
