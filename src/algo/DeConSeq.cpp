c624f4acb4dfa4f5cd1afda340195c9e561f6a6d
Boris Nagaev <bnagaev@gmail.com>
2013-03-07 13:59:21 +0400

    DeConSeq: fix. Fragment positions were used instead of sequence positions

--- src/algo/DeConSeq.cpp
+++ src/algo/DeConSeq.cpp
@@ -33,12 +33,14 @@ bool DeConSeq::run_impl() const {
             BOOST_ASSERT_MSG(seq, "Sequence must be a consensus of a block");
             int seq_block_length = seq_block->alignment_length();
             BOOST_FOREACH (const Fragment* seq_f, *seq_block) {
-                size_t begin_pos = fragment_pos(seq_f, fragment->begin_pos(),
-                                                seq_block_length);
-                size_t last_pos = fragment_pos(seq_f, fragment->last_pos(),
+                size_t fr_begin = fragment_pos(seq_f, fragment->begin_pos(),
                                                seq_block_length);
+                size_t fr_last = fragment_pos(seq_f, fragment->last_pos(),
+                                              seq_block_length);
+                size_t seq_begin = frag_to_seq(seq_f, fr_begin);
+                size_t seq_last = frag_to_seq(seq_f, fr_last);
                 Fragment* new_fragment = new Fragment(seq_f->seq());
-                new_fragment->set_begin_last(begin_pos, last_pos);
+                new_fragment->set_begin_last(seq_begin, seq_last);
                 new_block->insert(new_fragment);
             }
         }
