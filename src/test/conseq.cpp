224d9cb4ab0c347bd496286465c66c1494282eb2
Boris Nagaev <bnagaev@gmail.com>
2013-03-11 12:37:14 +0400

    conseq/deconseq test: test with alignment rows

--- src/test/conseq.cpp
+++ src/test/conseq.cpp
@@ -9,6 +9,7 @@
 #include <boost/foreach.hpp>
 
 #include "Sequence.hpp"
+#include "AlignmentRow.hpp"
 #include "Fragment.hpp"
 #include "Block.hpp"
 #include "BlockSet.hpp"
@@ -67,6 +68,54 @@ BOOST_AUTO_TEST_CASE (ConSeq_main) {
         }
         BOOST_CHECK((near(min_pos, 0) && near(max_pos, 2) && ori == 1) ||
                     (near(min_pos, 4) && near(max_pos, 6) && ori == -1));
+    }
+}
+
+BOOST_AUTO_TEST_CASE (ConSeq_alignment) {
+    using namespace bloomrepeats;
+    SequencePtr s1 = boost::make_shared<CompactSequence>("-caggccgg");
+    SequencePtr s2 = boost::make_shared<CompactSequence>("-caggctg-");
+    SequencePtr s3 = boost::make_shared<CompactSequence>("gctggatg-");
+    Fragment* f1 = new Fragment(s1, 0, s1->size() - 1);
+    Fragment* f2 = new Fragment(s2, 0, s2->size() - 1);
+    Fragment* f3 = new Fragment(s3, 1, s3->size() - 1);
+    f1->set_row(new CompactAlignmentRow("caggccgg"));
+    f2->set_row(new CompactAlignmentRow("caggctg-"));
+    f3->set_row(new CompactAlignmentRow("ctggatg-"));
+    Block* block = new Block;
+    block->insert(f1);
+    block->insert(f2);
+    block->insert(f3);
+    BlockSetPtr block_set = new_bs();
+    block_set->insert(block);
+    ConSeq conseq(block_set);
+    conseq.set_empty_block_set();
+    conseq.run();
+    BOOST_REQUIRE(conseq.block_set()->seqs().size() == 1);
+    SequencePtr new_seq = conseq.block_set()->seqs()[0];
+    BOOST_CHECK(new_seq->contents() == "caggctgg");
+    Fragment* f4 = new Fragment(new_seq, 0, 2); // cag
+    Fragment* f5 = new Fragment(new_seq, 4, 6, -1); // cag
+    Block* block_l2 = new Block;
+    block_l2->insert(f4);
+    block_l2->insert(f5);
+    BlockSetPtr block_set_l2 = new_bs();
+    block_set_l2->insert(block_l2);
+    DeConSeq deconseq(block_set_l2);
+    deconseq.set_empty_block_set();
+    deconseq.run();
+    BOOST_REQUIRE(deconseq.block_set()->size() == 1);
+    Block* slice = *deconseq.block_set()->begin();
+    BOOST_FOREACH (Fragment* f, *slice) {
+        size_t min_pos = f->min_pos();
+        size_t max_pos = f->max_pos();
+        int ori = f->ori();
+        if (f->seq() == s3.get()) {
+            min_pos -= 1;
+            max_pos -= 1;
+        }
+        BOOST_CHECK((min_pos == 0 && max_pos == 2 && ori == 1) ||
+                    (min_pos == 4 && max_pos == 6 && ori == -1));
     }
 }
 
