dbf257739cfd3d7f5d3d682223e6527be17b2689
Boris Nagaev <bnagaev@gmail.com>
2013-03-18 13:48:45 +0400

    SizeLimits: add fields max_spreading, min_identity, max_gaps

--- src/algo/SizeLimits.cpp
+++ src/algo/SizeLimits.cpp
@@ -10,7 +10,8 @@
 namespace bloomrepeats {
 
 SizeLimits::SizeLimits(int min_fragment_length, int min_block_size):
-    min_fragment_length_(min_fragment_length), min_block_size_(min_block_size)
+    min_fragment_length_(min_fragment_length), min_block_size_(min_block_size),
+    max_spreading_(0.2), min_identity_(0.9), max_gaps_(0.2)
 { }
 
 void SizeLimits::add_options_impl(po::options_description& desc) const {
@@ -19,12 +20,21 @@ void SizeLimits::add_options_impl(po::options_description& desc) const {
      "Minimal length of fragments in result")
     ("min-block", po::value<size_t>()->default_value(min_block_size()),
      "Minimal size of blocks in result")
+    ("max-spreading", po::value<float>()->default_value(max_spreading()),
+     "Fragment length spreading ((max - min) / avg)")
+    ("min-identity", po::value<float>()->default_value(min_identity()),
+     "Minimal block identity (on non-gap columns, only if alignment is known)")
+    ("max-gaps", po::value<float>()->default_value(max_gaps()),
+     "Max gap columns percentage (only if alignment is known)")
    ;
 }
 
 void SizeLimits::apply_options_impl(const po::variables_map& vm) {
     set_min_fragment_length(vm["min-fragment"].as<size_t>());
     set_min_block_size(vm["min-block"].as<size_t>());
+    set_max_spreading(vm["max-spreading"].as<float>());
+    set_min_identity(vm["min-identity"].as<float>());
+    set_max_gaps(vm["max-gaps"].as<float>());
 }
 
 }
--- src/algo/SizeLimits.hpp
+++ src/algo/SizeLimits.hpp
@@ -38,6 +38,46 @@ public:
         min_block_size_ = min_block_size;
     }
 
+    /** Max fragment length spreading ((max - min) / avg) */
+    float max_spreading() const {
+        return max_spreading_;
+    }
+
+    /** Set max fragment length spreading.
+    Defaults to 0.2.
+    */
+    void set_max_spreading(float max_spreading) {
+        max_spreading_ = max_spreading;
+    }
+
+    /** Return min identity on non-gap columns.
+    Should be applied only if alignment rows are known.
+    */
+    float min_identity() const {
+        return min_identity_;
+    }
+
+    /** Set min identity on non-gap columns.
+    Defaults to 0.9.
+    */
+    void set_min_identity(float min_identity) {
+        min_identity_ = min_identity;
+    }
+
+    /** Return max percentage of columns with gaps.
+    Should be applied only if alignment rows are known.
+    */
+    float max_gaps() const {
+        return max_gaps_;
+    }
+
+    /** Set min identity.
+    Defaults to 0.2.
+    */
+    void set_max_gaps(float max_gaps) {
+        max_gaps_ = max_gaps;
+    }
+
 protected:
     void add_options_impl(po::options_description& desc) const;
 
@@ -46,6 +86,9 @@ protected:
 private:
     int min_fragment_length_;
     int min_block_size_;
+    float max_spreading_; // fragment length spreading ((max - min) / avg)
+    float min_identity_; // only if alignment rows are known
+    float max_gaps_; // only if alignment rows are known
 };
 
 }
