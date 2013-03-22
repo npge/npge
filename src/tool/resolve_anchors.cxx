fcf31aca68b0f1ac1897c57629a3cbddc9be017a
Boris Nagaev <bnagaev@gmail.com>
2013-03-22 15:39:12 +0400

    add tool resolve_anchors

---
+++ src/tool/resolve_anchors.cxx
@@ -0,0 +1,33 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2012 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include "process.hpp"
+#include "Pipe.hpp"
+#include "AddSequences.hpp"
+#include "AddBlocks.hpp"
+#include "ResolveAnchors.hpp"
+#include "CheckNoOverlaps.hpp"
+#include "OutputPipe.hpp"
+
+using namespace bloomrepeats;
+
+class ResolveAnchorsPipe : public Pipe {
+public:
+    ResolveAnchorsPipe() {
+        add(new AddSequences, "target=other");
+        add(new AddBlocks, "target=other");
+        add(new ResolveAnchors);
+        add(new CheckNoOverlaps);
+        add(new OutputPipe);
+    }
+};
+
+int main(int argc, char** argv) {
+    return process(argc, argv, new ResolveAnchorsPipe,
+                   "Add anchors, resolve overlaps");
+}
+
