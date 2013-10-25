4bfb6ca0f072e0309fc3b2baed422324a689bbf6
Boris Nagaev <bnagaev@gmail.com>
2013-10-25 05:05:08 +0400

    use thread_group in BlocksJobs

--- src/algo/BlocksJobs.cpp
+++ src/algo/BlocksJobs.cpp
@@ -6,16 +6,99 @@
  */
 
 #include <vector>
-#include <boost/bind.hpp>
-#include <boost/thread.hpp>
-#include <boost/thread/mutex.hpp>
+#include <boost/cast.hpp>
 
 #include "BlocksJobs.hpp"
 #include "BlockSet.hpp"
+#include "thread_group.hpp"
 
 namespace bloomrepeats {
 
 typedef std::vector<Block*> BlocksVector;
+
+class BlockGroup : public ThreadGroup {
+public:
+    bool changed_;
+
+    BlockGroup(const BlocksJobs* jobs, const std::string& block_set_name):
+        changed_(false), jobs_(jobs), bs_i_(0) {
+        BlockSetPtr target = jobs->get_bs(block_set_name);
+        BlocksVector _(target->begin(), target->end());
+        bs_.swap(_);
+    }
+
+    ThreadTask* create_task_impl(ThreadWorker* worker);
+
+    ThreadWorker* create_worker_impl();
+
+    void perform_impl(int workers) {
+        changed_ |= jobs_->change_blocks(bs_);
+        ThreadGroup::perform_impl(workers);
+        changed_ |= jobs_->finish_work();
+    }
+
+private:
+    const BlocksJobs* jobs_;
+    BlocksVector bs_;
+    int bs_i_;
+};
+
+class BlockWorker : public ThreadWorker {
+public:
+    bool changed_;
+
+    BlockWorker(const BlocksJobs* jobs, BlockGroup* group):
+        ThreadWorker(group), jobs_(jobs), changed_(false)
+    { }
+
+    void work_impl() {
+        changed_ |= jobs_->initialize_thread();
+        ThreadWorker::work_impl();
+        changed_ |= jobs_->finish_thread();
+    }
+
+    ~BlockWorker() {
+        // this is called from main thread
+        BlockGroup* tg;
+        tg = boost::polymorphic_downcast<BlockGroup*>(thread_group());
+        tg->changed_ |= changed_;
+    }
+
+private:
+    const BlocksJobs* jobs_;
+};
+
+class BlockTask : public ThreadTask {
+public:
+    BlockTask(Block* block, const BlocksJobs* jobs, BlockWorker* worker):
+        ThreadTask(worker), block_(block), jobs_(jobs)
+    { }
+
+    void run_impl() {
+        bool changed = jobs_->apply_to_block(block_);
+        BlockWorker* w = boost::polymorphic_downcast<BlockWorker*>(worker());
+        w->changed_ |= changed;
+    }
+
+private:
+    Block* block_;
+    const BlocksJobs* jobs_;
+};
+
+ThreadTask* BlockGroup::create_task_impl(ThreadWorker* worker) {
+    if (bs_i_ < bs_.size()) {
+        Block* block = bs_[bs_i_];
+        bs_i_ += 1;
+        BlockWorker* w = boost::polymorphic_downcast<BlockWorker*>(worker);
+        return new BlockTask(block, jobs_, w);
+    } else {
+        return 0;
+    }
+}
+
+ThreadWorker* BlockGroup::create_worker_impl() {
+    return new BlockWorker(jobs_, this);
+}
 
 BlocksJobs::BlocksJobs(const std::string& block_set_name):
     block_set_name_(block_set_name)
@@ -41,52 +124,10 @@ bool BlocksJobs::finish_work() const {
     return finish_work_impl();
 }
 
-typedef BlocksVector::const_iterator It;
-
-static void process_blocks(It& it, const It& end, boost::mutex& mutex,
-                           const BlocksJobs* jobs, bool& result) {
-    bool changed = false;
-    changed |= jobs->initialize_thread();
-    while (true) {
-        Block* block;
-        {
-            boost::mutex::scoped_lock lock(mutex);
-            if (it != end) {
-                block = *it;
-                ++it;
-            } else {
-                break;
-            }
-        }
-        changed |= jobs->apply_to_block(block);
-    }
-    changed |= jobs->finish_thread();
-    if (changed) {
-        boost::mutex::scoped_lock lock(mutex);
-        result = true;
-    }
-}
-
 bool BlocksJobs::run_impl() const {
-    bool result = false;
-    BlockSetPtr target = get_bs(block_set_name());
-    BlocksVector bs(target->begin(), target->end());
-    result |= change_blocks(bs);
-    It it = bs.begin();
-    const It end = bs.end();
-    boost::mutex mutex;
-    boost::thread_group threads;
-    for (int i = 1; i < workers(); i++) {
-        using namespace boost;
-        threads.create_thread(bind(process_blocks, ref(it), ref(end),
-                                   ref(mutex), this, ref(result)));
-    }
-    process_blocks(it, end, mutex, this, result);
-    threads.join_all();
-    if (finish_work()) {
-        result = true;
-    }
-    return result;
+    BlockGroup block_group(this, block_set_name());
+    block_group.perform(workers());
+    return block_group.changed_;
 }
 
 bool BlocksJobs::change_blocks_impl(BlocksVector& blocks) const {
