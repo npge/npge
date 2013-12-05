68f99cf5afbc8b1a473c540c939b5ab66bd824f3
Boris Nagaev <bnagaev@gmail.com>
2013-12-06 03:41:45 +0400

    util: add macro TO_S (lexical_cast to string)

---
+++ src/util/to_s.hpp
@@ -0,0 +1,17 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2013 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef BR_TOS_HPP_
+#define BR_TOS_HPP_
+
+#include <string>
+#include <boost/lexical_cast.hpp>
+
+#define TO_S(x) boost::lexical_cast<std::string>(x)
+
+#endif
+
