c8840581542d3a758aa938497dc2778a9641a503
Boris Nagaev <bnagaev@gmail.com>
2013-12-18 13:47:03 +0400

    describe members of hit.hpp

--- src/algo/hit.hpp
+++ src/algo/hit.hpp
@@ -12,11 +12,20 @@
 
 namespace bloomrepeats {
 
+/** Set of fragments */
 typedef std::set<Fragment*, FragmentCompare> FragmentsSet;
+
+/** Collection of fragments */
 typedef FragmentCollection<Fragment*, FragmentsSet> S2F;
 
+/** Return if hit is internal for block set (s2f).
+This means, each fragment of hit is a subfragment of one fragment
+from s2f and size of corresponding block from blockset is
+less than size of hit.
+*/
 bool is_internal_hit(const S2F& s2f, const Block* hit);
 
+/** Return if block contain self-overlapping fragments */
 bool has_self_overlaps(Block* block);
 
 }
