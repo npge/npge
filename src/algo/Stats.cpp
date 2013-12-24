f5f33f48aae1f8e33e82c60c03c617c551f41350
Boris Nagaev <bnagaev@gmail.com>
2013-12-24 13:03:26 +0400

    Stats: if all values in list are equal, print all=<value>

--- src/algo/Stats.cpp
+++ src/algo/Stats.cpp
@@ -82,12 +82,19 @@ template<typename Vector>
 static void report_list(std::ostream& o, const Vector& list) {
     o << " number=" << list.size();
     if (!list.empty()) {
-        o << " min=" << *std::min_element(list.begin(), list.end());
-        o << " median=" << median_element(list);
-        double avg = avg_element_double(list);
-        boost::format double_2("%.2f");
-        o << " avg=" << str(double_2 % avg);
-        o << " max=" << *std::max_element(list.begin(), list.end());
+        typename Vector::value_type min_value, max_value;
+        min_value = *std::min_element(list.begin(), list.end());
+        max_value = *std::max_element(list.begin(), list.end());
+        if (min_value == max_value) {
+            o << " all=" << min_value;
+        } else {
+            o << " min=" << min_value;
+            o << " median=" << median_element(list);
+            double avg = avg_element_double(list);
+            boost::format double_2("%.2f");
+            o << " avg=" << str(double_2 % avg);
+            o << " max=" << max_value;
+        }
     }
     o << std::endl;
 }
