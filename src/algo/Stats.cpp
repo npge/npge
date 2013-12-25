ad38eec42aeac8fac3550c04f26d65db60889faa
Boris Nagaev <bnagaev@gmail.com>
2013-12-25 13:27:01 +0400

    move function report_list from Stats to util/report_list.hpp

--- src/algo/Stats.cpp
+++ src/algo/Stats.cpp
@@ -7,8 +7,6 @@
 
 #include <vector>
 #include <ostream>
-#include <algorithm>
-#include <boost/format.hpp>
 
 #include "Stats.hpp"
 #include "Sequence.hpp"
@@ -20,6 +18,7 @@
 #include "boundaries.hpp"
 #include "block_stat.hpp"
 #include "throw_assert.hpp"
+#include "report_list.hpp"
 
 namespace bloomrepeats {
 
@@ -43,27 +42,6 @@ typedef Boundaries Integers;
 bool fragment_has_overlaps(const Fragment* f) {
     return (f->next() && f->common_positions(*f->next())) ||
            (f->prev() && f->common_positions(*f->prev()));
-}
-
-template<typename Vector>
-static void report_list(std::ostream& o, const Vector& list) {
-    o << " number=" << list.size();
-    if (!list.empty()) {
-        typename Vector::value_type min_value, max_value;
-        min_value = *std::min_element(list.begin(), list.end());
-        max_value = *std::max_element(list.begin(), list.end());
-        if (min_value == max_value) {
-            o << " all=" << min_value;
-        } else {
-            o << " min=" << min_value;
-            o << " median=" << median_element(list);
-            double avg = avg_element_double(list);
-            boost::format double_2("%.2f");
-            o << " avg=" << str(double_2 % avg);
-            o << " max=" << max_value;
-        }
-    }
-    o << std::endl;
 }
 
 template<typename T1, typename T2>
---
+++ src/util/report_list.hpp
@@ -0,0 +1,44 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2013 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef BR_REPORT_LIST_HPP_
+#define BR_REPORT_LIST_HPP_
+
+#include <ostream>
+#include <algorithm>
+#include <boost/format.hpp>
+
+#include "boundaries.hpp"
+
+namespace bloomrepeats {
+
+/** Print properties of list of size_t or float */
+template<typename Vector>
+static void report_list(std::ostream& o, const Vector& list) {
+    o << " number=" << list.size();
+    if (!list.empty()) {
+        typename Vector::value_type min_value, max_value;
+        min_value = *std::min_element(list.begin(), list.end());
+        max_value = *std::max_element(list.begin(), list.end());
+        if (min_value == max_value) {
+            o << " all=" << min_value;
+        } else {
+            o << " min=" << min_value;
+            o << " median=" << median_element(list);
+            double avg = avg_element_double(list);
+            boost::format double_2("%.2f");
+            o << " avg=" << str(double_2 % avg);
+            o << " max=" << max_value;
+        }
+    }
+    o << std::endl;
+}
+
+}
+
+#endif
+
