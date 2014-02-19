8a46365c1e5b6e8afd19bc91f5f2ed3abae93797
Boris Nagaev <bnagaev@gmail.com>
2014-02-19 14:02:27 +0400

    AddBlastBlocks: do not pass short consensuses to blast

--- src/algo/AddBlastBlocks.cpp
+++ src/algo/AddBlastBlocks.cpp
@@ -5,6 +5,10 @@
  * See the LICENSE file for terms of use.
  */
 
+#include <algorithm>
+#include <boost/bind.hpp>
+#include <boost/foreach.hpp>
+
 #include "AddBlastBlocks.hpp"
 #include "SequencesFromOther.hpp"
 #include "Clear.hpp"
@@ -13,8 +17,32 @@
 #include "UniqueNames.hpp"
 #include "BlastFinder.hpp"
 #include "DeConSeq.hpp"
+#include "BlockSet.hpp"
+#include "Sequence.hpp"
 
 namespace bloomrepeats {
+
+class FilterSeqs : public Processor {
+public:
+    FilterSeqs() {
+        add_opt("blast-min-length", "min length of blast hit", 100);
+        // FIXME 100
+        add_opt_rule("blast-min-length >= 0");
+    }
+
+protected:
+    bool run_impl() const {
+        BlockSet& bs = *block_set();
+        std::vector<SequencePtr> seqs = bs.seqs();
+        int min_length = opt_value("blast-min-length").as<int>();
+        BOOST_FOREACH (const SequencePtr& seq, seqs) {
+            if (seq->size() < min_length) {
+                bs.remove_sequence(seq);
+            }
+        }
+        return true;
+    }
+};
 
 AddBlastBlocks::AddBlastBlocks(BlockSetPtr source):
     Pipe(source) {
@@ -23,6 +51,7 @@ AddBlastBlocks::AddBlastBlocks(BlockSetPtr source):
     add(new ConSeq, "target=consensus other=other");
     add(new RemoveNames, "target=consensus");
     add(new UniqueNames, "target=consensus");
+    add(new FilterSeqs, "target=consensus");
     add(new BlastFinder, "target=consensus");
     add(new DeConSeq, "target=target other=consensus");
 }
