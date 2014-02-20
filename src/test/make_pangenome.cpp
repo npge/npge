e3dbc11e3f601c5c214b83b31ba9b42558029698
Boris Nagaev <bnagaev@gmail.com>
2014-02-21 01:07:08 +0400

    add test make_pangenome for self-overlapping blocks case
    repeat TACG
    
    * check that make_pangenome does not go in infinite loop
    * check no (self)overlaps found

---
+++ src/test/make_pangenome.cpp
@@ -0,0 +1,36 @@
+/*
+ * bloomrepeats, Find genomic repeats, using Bloom filter based prefiltration
+ * Copyright (C) 2014 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <boost/test/unit_test.hpp>
+#include <boost/foreach.hpp>
+
+#include "Sequence.hpp"
+#include "Fragment.hpp"
+#include "Block.hpp"
+#include "MakePangenome.hpp"
+#include "hit.hpp"
+#include "CheckNoOverlaps.hpp"
+
+BOOST_AUTO_TEST_CASE (make_pangenome_TACG) {
+    using namespace bloomrepeats;
+    std::string TACG;
+    while (TACG.length() < 518) {
+        TACG += "TACG";
+    }
+    SequencePtr s1 = boost::make_shared<InMemorySequence>(TACG);
+    MakePangenome mp;
+    mp.set_options("--out-stats=/dev/null");
+    mp.block_set()->add_sequence(s1);
+    mp.run();
+    BOOST_FOREACH (Block* block, *mp.block_set()) {
+        BOOST_CHECK(!has_self_overlaps(block));
+    }
+    CheckNoOverlaps cno;
+    cno.set_block_set(mp.block_set());
+    cno.run();
+}
+
