a259f74f6c62e32a3a0991b238f80de5fb3469af
Boris Nagaev <bnagaev@gmail.com>
2014-02-23 14:52:44 +0400

    AddBlocks: check alignment row length >= fragment length
    fix several tests

--- src/algo/AddBlocks.cpp
+++ src/algo/AddBlocks.cpp
@@ -9,10 +9,14 @@
 #include <boost/foreach.hpp>
 
 #include "AddBlocks.hpp"
+#include "AlignmentRow.hpp"
+#include "Fragment.hpp"
+#include "Block.hpp"
 #include "BlockSet.hpp"
 #include "SeqStorage.hpp"
 #include "RowStorage.hpp"
 #include "read_block_set.hpp"
+#include "throw_assert.hpp"
 
 namespace bloomrepeats {
 
@@ -24,17 +28,27 @@ AddBlocks::AddBlocks():
 
 bool AddBlocks::run_impl() const {
     int size_before = block_set()->size();
+    std::vector<std::string> block_sets;
+    get_block_sets(block_sets);
     BOOST_FOREACH (std::istream& input_file, file_reader_) {
         BlockSetFastaReader reader(*block_set(), input_file,
                                    import_alignment(this),
                                    row_type(this),
                                    seq_type(this));
-        std::vector<std::string> block_sets;
-        get_block_sets(block_sets);
         BOOST_FOREACH (const std::string& bs_name, block_sets) {
             reader.set_block_set(bs_name, get_bs(bs_name).get());
         }
         reader.read_all_sequences();
+    }
+    if (import_alignment(this)) {
+        BOOST_FOREACH (const std::string& bs_name, block_sets) {
+            BOOST_FOREACH (Block* block, *get_bs(bs_name)) {
+                BOOST_FOREACH (Fragment* f, *block) {
+                    BOOST_ASSERT(f->row());
+                    BOOST_ASSERT(f->length() <= f->row()->length());
+                }
+            }
+        }
     }
     return block_set()->size() > size_before;
 }
