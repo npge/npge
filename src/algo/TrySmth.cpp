aa3ab0178583b1191518cb4d5a6218d68e917d5e
Boris Nagaev <bnagaev@gmail.com>
2014-02-25 14:28:49 +0400

    TrySmth: add blocks in chunks ordered by size desc

--- src/algo/TrySmth.cpp
+++ src/algo/TrySmth.cpp
@@ -5,14 +5,20 @@
  * See the LICENSE file for terms of use.
  */
 
+#include <boost/foreach.hpp>
+
 #include "TrySmth.hpp"
 #include "Union.hpp"
 #include "MetaProcessor.hpp"
 #include "Clear.hpp"
 #include "OverlaplessUnion.hpp"
 #include "Align.hpp"
+#include "Filter.hpp"
 #include "UniqueNames.hpp"
 #include "RemoveNames.hpp"
+#include "SizeLimits.hpp"
+#include "BlockSet.hpp"
+#include "Block.hpp"
 
 namespace bloomrepeats {
 
@@ -25,6 +31,31 @@ public:
     }
 };
 
+class AddingLoopBySize : public Processor {
+protected:
+    bool run_impl() const {
+        std::set<int> sizes;
+        BOOST_FOREACH (const Block* block, *other()) {
+            sizes.insert(block->size());
+        }
+        Union u(other());
+        Filter fil;
+        allow_everything(&fil);
+        AddingLoop al;
+        al.set_bs("target", block_set());
+        al.set_bs("other", u.block_set());
+        BOOST_REVERSE_FOREACH (int size, sizes) {
+            // set is ordered
+            u.block_set()->clear();
+            u.run();
+            fil.set_opt_value("min-block", size);
+            fil.set_opt_value("max-block", size);
+            fil.apply(u.block_set());
+            al.run();
+        }
+    }
+};
+
 TrySmth::TrySmth() {
     add(new Union, "target=smth-copy other=target");
     add(new UniqueNames, "target=smth-copy");
@@ -33,7 +64,8 @@ TrySmth::TrySmth() {
         " --remove-blocks-names=1");
     add(new Union, "target=smth-copy other=target");
     add(new Clear, "target=target");
-    add(new AddingLoop, "target=target other=smth-copy");
+    add(new AddingLoopBySize, "target=target other=smth-copy");
+    add(new UniqueNames, "target=target");
     add(new Clear, "target=smth-copy --clear-seqs=1");
 }
 
