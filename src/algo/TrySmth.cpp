f1daf7f848e75094f8d1ee517e9e5fe2380fc14b
Boris Nagaev <bnagaev@gmail.com>
2014-02-26 18:48:44 +0400

    TrySmth: pre-Filter testing set
    If some hit from testing set is bigger than old block,
    but after filtering it is equal to old block,
    then old block can be replaced with new one, but with other name.
    
    This commit removes bad blocks and bad fragments from blocks,
    so size of new block does not change in adding loops.

--- src/algo/TrySmth.cpp
+++ src/algo/TrySmth.cpp
@@ -71,7 +71,7 @@ TrySmth::TrySmth() {
     add(new MetaProcessor, "prefix|smth-");
     add(new RemoveNames, "target=target --remove-seqs-names=0 "
         " --remove-blocks-names=1");
-    add(new Union, "target=smth-copy other=target");
+    add(new Filter, "other=smth-copy target=target --good-to-other:=1");
     add(new Clear, "target=target");
     add(new AddingLoopBySize, "target=target other=smth-copy");
     add(new UniqueNames, "target=target");
