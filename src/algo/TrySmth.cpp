db4e3976e072aaa7e23413f0980270ed1593e0b2
Boris Nagaev <bnagaev@gmail.com>
2014-02-28 11:40:58 +0400

    TrySmth: improve adding loop
    Before, blocks bins were iterated by size, Filter'ed,
    passed through two OverlaplessUnion's.
    
    Now, special OverlaplessUnion-like processor is used.
    It deleted only blocks from other, overlapping with
    definitely better aligned blocks.
    If overlapping block from target is not aligned, blocks are left
    as are (wait until this block is aligned).
    Otherwise blocks are swapped: target <-> other.
    
    After this processor, target blocks are aligned and filtered
    (maybe subblock'ed). Then adding loop is repeated, untill
    other is empty.

--- src/algo/TrySmth.cpp
+++ src/algo/TrySmth.cpp
@@ -6,12 +6,14 @@
  */
 
 #include <boost/foreach.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <boost/tuple/tuple_comparison.hpp>
 
 #include "TrySmth.hpp"
 #include "Union.hpp"
 #include "MetaProcessor.hpp"
 #include "Clear.hpp"
-#include "OverlaplessUnion.hpp"
+#include "FragmentCollection.hpp"
 #include "Align.hpp"
 #include "Filter.hpp"
 #include "UniqueNames.hpp"
@@ -22,11 +24,82 @@
 
 namespace bloomrepeats {
 
+typedef std::set<Fragment*, FragmentCompare> FragmentsSet;
+typedef FragmentCollection<Fragment*, FragmentsSet> S2F;
+typedef std::vector<Block*> Blocks;
+
+struct BlockLengthLess {
+    bool operator()(Block* a, Block* b) const {
+        typedef boost::tuple<int, int, const std::string&> Tie;
+        return Tie(b->size(), b->alignment_length(), b->name())
+               <
+               Tie(a->size(), a->alignment_length(), a->name());
+    }
+};
+
+static bool has_alignment(const Block* block) {
+    BOOST_FOREACH (const Fragment* fragment, *block) {
+        if (!fragment->row()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+class SmthUnion : public Processor {
+protected:
+    bool run_impl() const {
+        BlockSet& t = *block_set();
+        BlockSet& o = *other();
+        S2F s2f;
+        s2f.add_bs(t);
+        Blocks blocks(o.begin(), o.end());
+        BlockLengthLess bll;
+        std::sort(blocks.begin(), blocks.end(), bll);
+        BOOST_FOREACH (Block* block, blocks) {
+            std::vector<Fragment*> o_f;
+            BOOST_FOREACH (Fragment* fragment, *block) {
+                s2f.find_overlap_fragments(o_f, fragment);
+            }
+            std::set<Block*> o_b;
+            BOOST_FOREACH (Fragment* f, o_f) {
+                o_b.insert(f->block());
+            }
+            bool swap = true;
+            bool remove = false;
+            BOOST_FOREACH (Block* b, o_b) {
+                if (!has_alignment(b)) {
+                    swap = false;
+                    break;
+                }
+                if (!bll(block, b)) {
+                    remove = true;
+                    swap = false;
+                    break;
+                }
+            }
+            if (swap) {
+                BOOST_FOREACH (Block* b, o_b) {
+                    s2f.remove_block(b);
+                    t.detach(b);
+                    o.insert(b);
+                }
+                s2f.add_block(block);
+                o.detach(block);
+                t.insert(block);
+            } else if (remove) {
+                o.erase(block);
+            }
+        }
+        return true;
+    }
+};
+
+
 class AddingLoop : public Pipe {
 public:
     AddingLoop() {
-        add(new OverlaplessUnion, "--ou-filter:=1");
-        add(new OverlaplessUnion, "--ou-move:=1");
+        add(new SmthUnion);
         add(new Align);
     }
 };
@@ -36,49 +109,13 @@ public:
     AddingLoopBySize() {
         al_ = new AddingLoop;
         al_->set_parent(this);
+        al_->set_options("target=target other=other", this);
     }
 
 protected:
     bool run_impl() const {
-        std::set<int> sizes;
-        BOOST_FOREACH (const Block* block, *other()) {
-            sizes.insert(block->size());
-        }
-        Filter fil;
-        allow_everything(&fil);
-        fil.set_opt_value("find-subblocks", false);
-        fil.set_opt_value("good-to-other", true);
-        fil.set_bs("target", other());
-        al_->set_bs("target", block_set());
-        al_->set_bs("other", fil.other());
-        Filter move_small;
-        move_small.set_opt_value("find-subblocks", false);
-        move_small.set_bs("target", block_set());
-        move_small.set_bs("other", other());
-        allow_everything(&move_small);
-        BOOST_REVERSE_FOREACH (int size, sizes) {
-            // set is ordered
-            fil.other()->clear();
-            fil.set_opt_value("min-block", size);
-            fil.set_opt_value("max-block", size);
-            fil.run();
-            while (!fil.other()->empty()) {
-                al_->run();
-                if (size >= 2) {
-                    // copy small subblocks back to other()
-                    move_small.set_opt_value("max-block", size - 1);
-                    move_small.set_opt_value("min-block", 1);
-                    move_small.set_opt_value("good-to-other", true);
-                    move_small.run();
-                    // remove small subblocks from target
-                    move_small.set_opt_value("good-to-other", false);
-                    move_small.set_opt_value("max-block", -1);
-                    move_small.set_opt_value("min-block", size);
-                    move_small.run();
-                    // TODO move blocks (not copy and delete)
-                    // TODO Filter's option to move good blocks
-                }
-            }
+        while (!other()->empty()) {
+            al_->run();
         }
     }
 
