41750be433fd045bcb20d7351576b51b5e6d8370
Boris Nagaev <bnagaev@gmail.com>
2014-03-14 01:18:24 +0400

    reimplement Consensus as ConSeq + Output

--- src/algo/Consensus.cpp
+++ src/algo/Consensus.cpp
@@ -5,27 +5,18 @@
  * See the LICENSE file for terms of use.
  */
 
-#include <boost/foreach.hpp>
-
 #include "Consensus.hpp"
-#include "Block.hpp"
-#include "BlockSet.hpp"
+#include "ConSeq.hpp"
+#include "Output.hpp"
+#include "Clear.hpp"
 
 namespace bloomrepeats {
 
-Consensus::Consensus():
-    file_writer_(this, "out-consensus", "Output file with consensus", true)
-{ }
-
-bool Consensus::run_impl() const {
-    std::ostream& out = file_writer_.output();
-    BOOST_FOREACH (Block* b, *block_set()) {
-        out << ">" << b->name();
-        out << std::endl;
-        b->consensus(out);
-        out << std::endl;
-    }
-    return false;
+Consensus::Consensus() {
+    add(new ConSeq, "target=cons other=target");
+    add(new Output, "target=cons prefix|cons- "
+        "--cons-dump-seq:=1 --cons-dump-block:=0");
+    add(new Clear, "target=cons --clear-blocks:=1 --clear-seqs:=1");
 }
 
 const char* Consensus::name_impl() const {
--- src/algo/Consensus.hpp
+++ src/algo/Consensus.hpp
@@ -8,26 +8,18 @@
 #ifndef BR_CONSENSUS_HPP_
 #define BR_CONSENSUS_HPP_
 
-#include "Processor.hpp"
-#include "FileWriter.hpp"
+#include "Pipe.hpp"
 
 namespace bloomrepeats {
 
-/** Write consensuses of all blocks to a file.
-\see ExternalAligner
-*/
-class Consensus : public Processor {
+/** Write consensuses of all blocks to a file */
+class Consensus : public Pipe {
 public:
     /** Constructor */
     Consensus();
 
 protected:
-    bool run_impl() const;
-
     const char* name_impl() const;
-
-private:
-    FileWriter file_writer_;
 };
 
 }
