34214b924d3d63155e127bac8491415e9907c0b4
Boris Nagaev <bnagaev@gmail.com>
2014-03-14 14:09:32 +0400

    BlockSetWidget: remove column 'pure gap'

--- src/gui/BlockSetWidget.cpp
+++ src/gui/BlockSetWidget.cpp
@@ -16,7 +16,6 @@ enum {
     FRAGMENTS_C, COLUMNS_C,
     IDENT_NOGAP_C, IDENT_GAP_C,
     NOIDENT_NOGAP_C, NOIDENT_GAP_C,
-    PURE_GAP_C,
     IDENTITY_C, GC_C
 };
 
@@ -30,7 +29,6 @@ public:
         columns_ << tr("fragments") << tr("columns");
         columns_ << tr("ident-nogap") << tr("ident-gap");
         columns_ << tr("noident-nogap") << tr("noident-gap");
-        columns_ << tr("pure gap");
         columns_ << tr("% identity") << tr("% GC");
     }
 
@@ -57,8 +55,6 @@ public:
                     return stat->noident_nogap();
                 } else if (index.column() == NOIDENT_GAP_C) {
                     return stat->noident_gap();
-                } else if (index.column() == PURE_GAP_C) {
-                    return stat->pure_gap();
                 } else if (index.column() == IDENTITY_C) {
                     double id = block_identity(*stat) * 100;
                     return QString::number(id, 'f', 1);
