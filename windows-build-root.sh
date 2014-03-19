b411aae046c79f334dcac032b1acf3e89a50b1f1
Boris Nagaev <bnagaev@gmail.com>
2014-03-19 14:38:16 +0400

    add script to cross-compile from linux to win32 using mxe

---
+++ windows-build-root.sh
@@ -0,0 +1,7 @@
+sudo apt-get install autoconf automake bash bison bzip2 \
+    cmake flex gettext git g++ intltool \
+    libffi-dev libtool libltdl-dev libssl-dev \
+    libxml-parser-perl make openssl patch perl \
+    pkg-config scons sed unzip wget xz-utils \
+    mercurial
+
---
+++ windows-build.sh
@@ -0,0 +1,14 @@
+# create build environment and cross compile to static EXE file
+
+git clone -b stable https://github.com/mxe/mxe.git
+cd mxe
+make boost
+make qt
+cd ..
+
+hg clone https://bitbucket.org/starius/bloomrepeats
+mkdir br-build
+cd br-build
+cmake -DCMAKE_TOOLCHAIN_FILE=../mxe/usr/i686-pc-mingw32.static/share/cmake/mxe-conf.cmake ../bloomrepeats/
+make
+
