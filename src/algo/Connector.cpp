8466307606f2b8c304a01b3c462c9bde7414c8bd
Boris Nagaev <bnagaev@gmail.com>
2014-04-06 23:25:31 +0400

    Connector: add option --connect-circular connecting last->first

--- src/algo/Connector.cpp
+++ src/algo/Connector.cpp
@@ -8,6 +8,7 @@
 #include <boost/foreach.hpp>
 
 #include "Connector.hpp"
+#include "Sequence.hpp"
 #include "Fragment.hpp"
 #include "Block.hpp"
 #include "BlockSet.hpp"
@@ -16,6 +17,8 @@ namespace bloomrepeats {
 
 Connector::Connector() {
     declare_bs("target", "Target blockset");
+    add_opt("connect-circular", "Connect last and first fragments "
+            "of circular chromosomes", false);
 }
 
 static struct FragmentCompare {
@@ -25,19 +28,25 @@ static struct FragmentCompare {
 } fragment_compare;
 
 void Connector::run_impl() const {
+    bool circular = opt_value("connect-circular").as<bool>();
     typedef std::vector<Fragment*> Fs;
     typedef std::map<Sequence*, Fs> Seq2Fs;
     Seq2Fs seq2fs;
     BOOST_FOREACH (Block* block, *block_set()) {
         BOOST_FOREACH (Fragment* fragment, *block) {
             seq2fs[fragment->seq()].push_back(fragment);
+            fragment->disconnect();
         }
     }
     BOOST_FOREACH (Seq2Fs::value_type& seq_and_fs, seq2fs) {
+        Sequence* seq = seq_and_fs.first;
         Fs& fs = seq_and_fs.second;
         std::sort(fs.begin(), fs.end(), fragment_compare);
         for (int i = 1; i < fs.size(); i++) {
             Fragment::connect(fs[i - 1], fs[i]);
+        }
+        if (circular && seq->circular()) {
+            Fragment::connect(fs.back(), fs.front());
         }
     }
 }
