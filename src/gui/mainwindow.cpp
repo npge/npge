31725913dd3ac98f3683b1cb8233c49f06075f3a
Boris Nagaev <bnagaev@gmail.com>
2014-04-11 15:55:25 +0400

    update gui to use standard files produced by postprocessing script

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -7,13 +7,11 @@
 #include "BlockSetWidget.hpp"
 #include "BlockSet.hpp"
 #include "block_set_alignment.hpp"
-#include "Block.hpp"
 #include "config.hpp"
-#include "AddGenes.hpp"
 
 using namespace bloomrepeats;
 
-BlockSetPtr test_bs;
+BlockSetPtr pangenome_bs;
 BlockSetPtr genes_bs;
 BlockSetPtr split_parts;
 BlockSetPtr low_similarity;
@@ -24,30 +22,29 @@ MainWindow::MainWindow(QWidget* parent) :
     ui->setupUi(this);
     showMaximized();
     //
-    test_bs = new_bs();
-    std::ifstream test_file("test.fasta");
-    test_file >> *test_bs;
+    pangenome_bs = new_bs();
+    std::ifstream pangenome_file("pangenome-merged.fasta");
+    pangenome_file >> *pangenome_bs;
     //
-    AddGenes ag;
-    genes_bs = ag.block_set();
-    genes_bs->add_sequences(test_bs->seqs());
-    ag.set_opt_value("in-genes", std::string("test.genes"));
-    ag.run();
+    genes_bs = new_bs();
+    genes_bs->add_sequences(pangenome_bs->seqs());
+    std::ifstream genes_file("genes.fasta");
+    genes_file >> *genes_bs;
     //
     split_parts = new_bs();
-    split_parts->add_sequences(test_bs->seqs());
-    std::ifstream split_file("split.fasta");
+    split_parts->add_sequences(pangenome_bs->seqs());
+    std::ifstream split_file("pangenome-merged-split.fasta");
     split_file >> *split_parts;
     //
     low_similarity = new_bs();
-    low_similarity->add_sequences(test_bs->seqs());
-    std::ifstream low_file("low.fasta");
+    low_similarity->add_sequences(pangenome_bs->seqs());
+    std::ifstream low_file("pangenome-merged-low.fasta");
     low_file >> *low_similarity;
     //
-    std::ifstream test_bsaln("test.bsaln");
-    bsa_input(*test_bs, test_bsaln);
+    std::ifstream test_bsaln("pangenome-merged.bsa");
+    bsa_input(*pangenome_bs, test_bsaln);
     //
-    BlockSetWidget* bsw = new BlockSetWidget(test_bs);
+    BlockSetWidget* bsw = new BlockSetWidget(pangenome_bs);
     bsw->set_genes(genes_bs);
     bsw->set_split_parts(split_parts);
     bsw->set_low_similarity(low_similarity);
