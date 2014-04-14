c203065338c4813084f1658548223e5a5f370e50
Boris Nagaev <bnagaev@gmail.com>
2014-04-14 14:20:44 +0400

    gui: use QErrorMessage for caught exceptions

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,8 +1,29 @@
-#include <QApplication>
+#include <exception>
+#include <QtGui>
 #include "mainwindow.hpp"
 
+class MyApplication : public QApplication {
+public:
+    MyApplication(int argc, char* argv[]):
+        QApplication(argc, argv)
+    { }
+
+    bool notify(QObject* receiver, QEvent* e) {
+        try {
+            QApplication::notify(receiver, e);
+        } catch (const std::exception& e) {
+            QString what = QString::fromStdString(e.what());
+            QString error = "<b>The error occured</b>.<br><br>"
+                            "Description for developers:<br><br>" + what;
+            QErrorMessage::qtHandler()->resize(400, 300);
+            QErrorMessage::qtHandler()->showMessage(error);
+            return false;
+        }
+    }
+};
+
 int main(int argc, char* argv[]) {
-    QApplication a(argc, argv);
+    MyApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
