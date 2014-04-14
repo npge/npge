9f561be80943e9b4c2f3f30607a3b4929fbc1e9e
Boris Nagaev <bnagaev@gmail.com>
2014-04-14 23:46:10 +0400

    throw_assert.cpp: reduce source file name
    src/model/Fragment.cpp instead of /path/to/build/src/model/Fragment.cpp

--- src/util/throw_assert.cpp
+++ src/util/throw_assert.cpp
@@ -5,16 +5,29 @@
  * See the LICENSE file for terms of use.
  */
 
+#include <cstring>
 #include <stdexcept>
 #include <sstream>
 #include "throw_assert.hpp"
 
 namespace boost {
 
+#define SRC_PATTERN "src/"
+
+const char* reduce_path(const char* file) {
+    const char* subpath = strstr(file, SRC_PATTERN);
+    if (subpath) {
+        subpath += strlen(SRC_PATTERN);
+        return subpath;
+    } else {
+        return file;
+    }
+}
+
 void assertion_failed(char const* expr, char const* function,
                       char const* file, long line) {
     std::stringstream err;
-    err << file << ":" << line << ": " << function << ": ";
+    err << reduce_path(file) << ":" << line << ": " << function << ": ";
     err << "Assertation `" << expr << "' failed.";
     throw std::logic_error(err.str());
 }
@@ -23,7 +36,7 @@ void assertion_failed_msg(char const* expr, char const* msg,
                           char const* function,
                           char const* file, long line) {
     std::stringstream err;
-    err << file << ":" << line << ": " << function << ": ";
+    err << reduce_path(file) << ":" << line << ": " << function << ": ";
     err << "Assertation `" << expr << "' failed." << std::endl;
     err << "Error message `" << msg << "'.";
     throw std::logic_error(err.str());
