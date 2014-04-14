40d47ba5dd1f55515e07e054fbe032f74519ee46
Boris Nagaev <bnagaev@gmail.com>
2014-04-15 01:42:35 +0400

    throw_assert: add wrappers of boost_assert_msg

--- src/util/throw_assert.hpp
+++ src/util/throw_assert.hpp
@@ -8,4 +8,35 @@
 #define BOOST_ENABLE_ASSERT_HANDLER
 
 #include "boost-assert.hpp"
+#include "to_s.hpp"
+
+#define ASSERT_TRUE(exp_a) \
+    BOOST_ASSERT_MSG(exp_a, TO_S(exp_a).c_str())
+
+#define ASSERT_FALSE(exp_a) \
+    BOOST_ASSERT_MSG(!(exp_a), ("!" + TO_S(exp_a)).c_str())
+
+#define ASSERT_EQ(exp_a, exp_b) \
+    BOOST_ASSERT_MSG((exp_a) == (exp_b), \
+        (TO_S(exp_a) + " == " + TO_S(exp_b)).c_str())
+
+#define ASSERT_NE(exp_a, exp_b) \
+    BOOST_ASSERT_MSG((exp_a) != (exp_b), \
+        (TO_S(exp_a) + " != " + TO_S(exp_b)).c_str())
+
+#define ASSERT_LT(exp_a, exp_b) \
+    BOOST_ASSERT_MSG((exp_a) < (exp_b), \
+        (TO_S(exp_a) + " < " + TO_S(exp_b)).c_str())
+
+#define ASSERT_LTE(exp_a, exp_b) \
+    BOOST_ASSERT_MSG((exp_a) <= (exp_b), \
+        (TO_S(exp_a) + " <= " + TO_S(exp_b)).c_str())
+
+#define ASSERT_GT(exp_a, exp_b) \
+    BOOST_ASSERT_MSG((exp_a) > (exp_b), \
+        (TO_S(exp_a) + " > " + TO_S(exp_b)).c_str())
+
+#define ASSERT_GTE(exp_a, exp_b) \
+    BOOST_ASSERT_MSG((exp_a) >= (exp_b), \
+        (TO_S(exp_a) + " >= " + TO_S(exp_b)).c_str())
 
