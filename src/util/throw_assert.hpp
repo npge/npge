33fe43bac0c76038e54540e584640d2e819deba2
Boris Nagaev <bnagaev@gmail.com>
2014-04-14 23:37:54 +0400

    throw_assert.hpp: include not only once
    BOOST_ASSERT_MSG did not work in GUI with NDEGUG because of
    previous inclusions.

--- src/util/throw_assert.hpp
+++ src/util/throw_assert.hpp
@@ -5,12 +5,7 @@
  * See the LICENSE file for terms of use.
  */
 
-#ifndef BR_THROW_ASSERT_HPP_
-#define BR_THROW_ASSERT_HPP_
-
 #define BOOST_ENABLE_ASSERT_HANDLER
 
 #include "boost-assert.hpp"
-
-#endif
 
