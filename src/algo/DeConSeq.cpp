88ba20abd07c7633b2c3f7494cc279a61f4d0dc9
Boris Nagaev <bnagaev@gmail.com>
2014-04-29 18:15:45 +0400

    DeConSeq: preserve alignment if possible; test

--- src/algo/DeConSeq.cpp
+++ src/algo/DeConSeq.cpp
@@ -13,6 +13,7 @@
 #include "BlockSet.hpp"
 #include "Block.hpp"
 #include "Fragment.hpp"
+#include "AlignmentRow.hpp"
 #include "convert_position.hpp"
 #include "throw_assert.hpp"
 #include "Exception.hpp"
@@ -23,6 +24,26 @@ DeConSeq::DeConSeq(const BlockSetPtr& source) {
     set_other(source);
     declare_bs("target", "Where new blocks are added");
     declare_bs("other", "From where consensus blocks are taken");
+}
+
+static void deconseq_row(const Fragment* fragment, Fragment* f) {
+    AlignmentRow* row = fragment->row();
+    boost::scoped_ptr<AlignmentRow> tmp_row((f->detach_row()));
+    ASSERT_TRUE(row);
+    ASSERT_TRUE(tmp_row);
+    AlignmentRow* new_row = AlignmentRow::new_row(row->type());
+    f->set_row(new_row);
+    int length = row->length();
+    new_row->set_length(length);
+    for (int i = 0; i < length; i++) {
+        int tmp_pos = row->map_to_fragment(i);
+        if (tmp_pos != -1) {
+            int new_pos = tmp_row->map_to_fragment(tmp_pos);
+            if (new_pos != -1) {
+                new_row->bind(new_pos, i);
+            }
+        }
+    }
 }
 
 Block* DeConSeq::deconseq_block(const Block* block) {
@@ -38,10 +59,15 @@ Block* DeConSeq::deconseq_block(const Block* block) {
         }
         int start = fragment->begin_pos();
         int stop = fragment->last_pos();
+        bool alignment = (fragment->row() != 0);
         boost::scoped_ptr<Block> temp_block((seq_block->slice(start,
-                                             stop, /* alignment */ false)));
+                                             stop, alignment)));
         BOOST_FOREACH (Fragment* f, *temp_block) {
             new_block->insert(f); // fragment->block() == new_block
+            if (f->row()) {
+                ASSERT_TRUE(fragment->row());
+                deconseq_row(fragment, f);
+            }
         }
         // temp_block is deleted, but new fragments do not
     }
--- src/test/conseq.cpp
+++ src/test/conseq.cpp
@@ -5,6 +5,7 @@
  * See the LICENSE file for terms of use.
  */
 
+#include <algorithm>
 #include <boost/test/unit_test.hpp>
 #include <boost/foreach.hpp>
 
@@ -117,5 +118,70 @@ BOOST_AUTO_TEST_CASE (ConSeq_alignment) {
         BOOST_CHECK((min_pos == 0 && max_pos == 2 && ori == 1) ||
                     (min_pos == 4 && max_pos == 6 && ori == -1));
     }
+}
+
+BOOST_AUTO_TEST_CASE (DeConSeq_alignment) {
+    using namespace bloomrepeats;
+    SequencePtr s1 = boost::make_shared<CompactSequence>("-CAGGCCGG");
+    SequencePtr s2 = boost::make_shared<CompactSequence>("-CAGGCTG-");
+    SequencePtr s3 = boost::make_shared<CompactSequence>("GCTGGATG-");
+    s1->set_name("s1");
+    s2->set_name("s2");
+    s3->set_name("s3");
+    Fragment* f1 = new Fragment(s1, 0, s1->size() - 1);
+    Fragment* f2 = new Fragment(s2, 0, s2->size() - 1);
+    Fragment* f3 = new Fragment(s3, 0, s3->size() - 1);
+    f1->set_row(new CompactAlignmentRow("-CAGGCCGG"));
+    f2->set_row(new CompactAlignmentRow("-CAGGCTG-"));
+    f3->set_row(new CompactAlignmentRow("GCTGGATG-"));
+    Block* b = new Block;
+    b->insert(f1);
+    b->insert(f2);
+    b->insert(f3);
+    SequencePtr s4((new CompactSequence("GCAGAGCCGG")));
+    s4->set_name("s4");
+    Fragment* f4 = new Fragment(s4, 0, s4->size() - 1);
+    f4->set_row(new CompactAlignmentRow("GCAGAGCCGG"));
+    Block* ba = new Block;
+    ba->insert(f4);
+    BlockSetPtr block_set = new_bs();
+    block_set->insert(b);
+    block_set->insert(ba);
+    ConSeq conseq(block_set);
+    conseq.set_empty_block_set();
+    conseq.run();
+    BOOST_REQUIRE(conseq.block_set()->seqs().size() == 2);
+    SequencePtr new_seq = conseq.block_set()->seqs()[0];
+    SequencePtr new_seq_a = conseq.block_set()->seqs()[1];
+    if (new_seq->contents() == "GCAGAGCCGG") {
+        new_seq.swap(new_seq_a);
+    }
+    BOOST_REQUIRE(new_seq->contents() == "GCAGGCTGG");
+    BOOST_REQUIRE(new_seq_a->contents() == "GCAGAGCCGG");
+    Fragment* cf1 = new Fragment(new_seq, 0, new_seq->size() - 1);
+    Fragment* cf2 = new Fragment(new_seq_a, 0, new_seq_a->size() - 1);
+    cf1->set_row(new CompactAlignmentRow("GCAG-GCTGG"));
+    cf2->set_row(new CompactAlignmentRow("GCAGAGCCGG"));
+    Block* cb = new Block;
+    cb->insert(cf1);
+    cb->insert(cf2);
+    BlockSetPtr cbs = new_bs();
+    cbs->insert(cb);
+    DeConSeq deconseq(cbs);
+    deconseq.set_empty_block_set();
+    deconseq.run();
+    BlockSetPtr result = deconseq.block_set();
+    BOOST_REQUIRE(result->size() == 1);
+    Block* result_block = result->front();
+    BOOST_REQUIRE(result_block->size() == 4);
+    Strings aln;
+    BOOST_FOREACH (Fragment* f, *result_block) {
+        aln.push_back(f->str());
+    }
+    std::sort(aln.begin(), aln.end());
+    BOOST_CHECK(aln[0] == "-CAG-GCCGG");
+    BOOST_CHECK(aln[1] == "-CAG-GCTG-");
+    BOOST_CHECK(aln[2] == "GCAGAGCCGG");
+    BOOST_CHECK(aln[3] == "GCTG-GATG-");
 }
 
