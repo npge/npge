133da2ecf9f527055a4ab71f4c14d82229f505c4
Boris Nagaev <bnagaev@gmail.com>
2014-05-10 14:25:32 +0400

    TrySmth: optimize subblocks finding
    Do not recreate Rest, DeConSeq, consensus sequence and
    blocksets for each block.

--- src/algo/TrySmth.cpp
+++ src/algo/TrySmth.cpp
@@ -51,26 +51,50 @@ class SmthUnion : public Processor {
 private:
     BlockLengthLess bll;
     mutable S2F s2f;
-    mutable BlockSetPtr subblocks;
+    mutable BlockSetPtr cons_bs, rest_bs, subblocks;
+    mutable Rest rest;
+    mutable DeConSeq deconseq;
+    mutable SequencePtr cons;
 
 protected:
-    void run_impl() const {
+    void before_run() const {
         BlockSet& t = *block_set();
         BlockSet& o = *other();
         s2f.clear();
         s2f.add_bs(t);
+        cons.reset(new DummySequence);
+        rest.set_bs("other", new_bs());
+        rest.set_bs("target", new_bs());
+        cons_bs = rest.other();
+        rest_bs = rest.block_set();
+        deconseq.set_bs("other", rest_bs);
+        deconseq.set_bs("target", new_bs());
+        subblocks = deconseq.block_set();
+        subblocks->add_sequences(o.seqs());
+        cons_bs->add_sequence(cons);
+        rest_bs->add_sequence(cons);
+    }
+
+    void run_impl() const {
+        before_run();
+        BlockSet& o = *other();
         Blocks blocks(o.begin(), o.end());
         std::sort(blocks.begin(), blocks.end(), bll);
-        subblocks = new_bs();
-        subblocks->add_sequences(o.seqs());
         BOOST_FOREACH (Block* block, blocks) {
             process_block(block);
             ASSERT_FALSE(o.has(block));
         }
         ASSERT_TRUE(o.empty());
         o.swap(*subblocks);
+        after_run();
+    }
+
+    void after_run() const {
         s2f.clear();
-        subblocks.reset();
+        cons_bs->clear();
+        rest_bs->clear();
+        subblocks->clear();
+        cons.reset();
     }
 
     void process_block(Block* block) const {
@@ -92,28 +116,25 @@ protected:
     void split_block(Block* block) const {
         BlockSet& o = *other();
         int length = block->alignment_length();
-        Rest rest;
-        BlockSet& cons_bs = *rest.other();
-        DummySequence* cons = new DummySequence('N', length);
+        cons->set_size(length);
         cons->set_block(block, /* set_consensus */ false);
-        cons_bs.add_sequence(SequencePtr(cons));
         Block* cons_b = new Block;
-        cons_bs.insert(cons_b);
+        cons_bs->insert(cons_b);
+        Sequence* cons_ptr = cons.get();
         BOOST_FOREACH (Fragment* fragment, *block) {
             std::vector<Fragment> overlaps;
             s2f.find_overlaps(overlaps, fragment);
             BOOST_FOREACH (const Fragment& ol, overlaps) {
                 ASSERT_NE(ol, Fragment::INVALID);
                 ASSERT_GT(ol.length(), 0);
-                mark_bad(ol, fragment, cons_b, cons);
+                mark_bad(ol, fragment, cons_b, cons_ptr);
             }
         }
         rest.run();
-        DeConSeq deconseq;
-        deconseq.set_bs("other", rest.block_set());
-        deconseq.set_bs("target", subblocks);
         deconseq.run();
         o.erase(block);
+        cons_bs->clear_blocks();
+        rest_bs->clear_blocks();
     }
 
     void mark_bad(const Fragment& ol,
