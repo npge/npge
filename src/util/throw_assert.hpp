af2d7c5243da1e60dc33bd99be77a3f144e56c85
Boris Nagaev <bnagaev@gmail.com>
2014-05-18 11:21:01 +0400

    add configuration option BR_ASSERTS

--- src/util/throw_assert.hpp
+++ src/util/throw_assert.hpp
@@ -8,16 +8,25 @@
 #include <boost/current_function.hpp>
 
 #include "to_s.hpp"
+#include "config.hpp"
 
 namespace bloomrepeats {
 void assertion_failed_msg(char const* expr, char const* msg,
                           char const* function, char const* file, long line);
 }
 
+#ifdef BR_ASSERTS
+
 #define ASSERT_MSG(expr, msg) ((expr) \
     ? ((void)0) \
     : ::bloomrepeats::assertion_failed_msg(#expr, msg,\
         BOOST_CURRENT_FUNCTION, __FILE__, __LINE__))
+
+#else
+
+#define ASSERT_MSG(expr, msg) ((void)0)
+
+#endif
 
 #define ASSERT_TRUE(exp_a) \
     ASSERT_MSG(exp_a, TO_S(exp_a).c_str())
