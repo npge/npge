9052a8b6115c417062b63c3c196d56e6f6d6792f
Boris Nagaev <bnagaev@gmail.com>
2014-06-26 15:33:33 +0400

    to_s.hpp: add macro D_CAST

--- src/util/to_s.hpp
+++ src/util/to_s.hpp
@@ -10,8 +10,10 @@
 
 #include <string>
 #include <boost/lexical_cast.hpp>
+#include <boost/cast.hpp>
 
 #define L_CAST boost::lexical_cast
+#define D_CAST boost::polymorphic_downcast
 
 #define TO_S(x) L_CAST<std::string>(x)
 
