bb8e02e8412ab48f5532dcdd42185b87e95c5a78
Boris Nagaev <bnagaev@gmail.com>
2014-07-19 13:11:15 +0400

    gui: don't read additional files if fasta filename passed

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -29,33 +29,33 @@ MainWindow::MainWindow(int argc, char** argv,
     ui->setupUi(this);
     showMaximized();
     //
+    pangenome_bs = new_bs();
     std::string fname("pangenome-merged.fasta");
     if (argc >= 2) {
-        fname = argv[1];
+        std::ifstream pangenome_file(argv[1]);
+        pangenome_file >> *pangenome_bs;
+    } else {
+        std::ifstream pangenome_file("pangenome-merged.fasta");
+        pangenome_file >> *pangenome_bs;
+        //
+        genes_bs = new_bs();
+        genes_bs->add_sequences(pangenome_bs->seqs());
+        std::ifstream genes_file("genes.fasta");
+        genes_file >> *genes_bs;
+        //
+        split_parts = new_bs();
+        split_parts->add_sequences(pangenome_bs->seqs());
+        std::ifstream split_file("pangenome-merged-split.fasta");
+        split_file >> *split_parts;
+        //
+        low_similarity = new_bs();
+        low_similarity->add_sequences(pangenome_bs->seqs());
+        std::ifstream low_file("pangenome-merged-low.fasta");
+        low_file >> *low_similarity;
+        //
+        std::ifstream test_bsaln("pangenome-merged.bsa");
+        bsa_input(*pangenome_bs, test_bsaln);
     }
-    //
-    pangenome_bs = new_bs();
-    std::ifstream pangenome_file(fname.c_str());
-    pangenome_file >> *pangenome_bs;
-    //
-    genes_bs = new_bs();
-    genes_bs->add_sequences(pangenome_bs->seqs());
-    std::ifstream genes_file("genes.fasta");
-    genes_file >> *genes_bs;
-    //
-    split_parts = new_bs();
-    split_parts->add_sequences(pangenome_bs->seqs());
-    std::ifstream split_file("pangenome-merged-split.fasta");
-    split_file >> *split_parts;
-    //
-    low_similarity = new_bs();
-    low_similarity->add_sequences(pangenome_bs->seqs());
-    std::ifstream low_file("pangenome-merged-low.fasta");
-    low_file >> *low_similarity;
-    //
-    std::ifstream test_bsaln("pangenome-merged.bsa");
-    bsa_input(*pangenome_bs, test_bsaln);
-    //
     BlockSetWidget* bsw = new BlockSetWidget(pangenome_bs);
     bsw->set_genes(genes_bs);
     bsw->set_split_parts(split_parts);
