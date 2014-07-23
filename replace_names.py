98518763acb91cd663cce098a782896dea5c6acc
Boris Nagaev <bnagaev@gmail.com>
2014-07-23 15:15:39 +0400

    replace_names.py: remove trailing spaces after description

--- replace_names.py
+++ replace_names.py
@@ -20,7 +20,7 @@ def replace_names(args):
             for k, v in name2name.items():
                 if k in name:
                     name = v
-                    descr = 'ac=' + k + ' ' + descr
+                    descr = 'ac=' + k + ' ' + descr.strip()
                     break
             if name == line[1:].split(" ", 1)[0]:
                 print 'Warning: name ' + name + ' was not replaced'
