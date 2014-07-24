0e6edbc0cc099a4972b872010148b390cc504fdf
Boris Nagaev <bnagaev@gmail.com>
2014-07-24 12:42:23 +0400

    make_hash() is now inline function

--- src/util/make_hash.hpp
+++ src/util/make_hash.hpp
@@ -8,18 +8,39 @@
 #ifndef NPGE_MAKE_HASH_HPP_
 #define NPGE_MAKE_HASH_HPP_
 
+#include <algorithm>
+#include <boost/utility/binary.hpp>
+
+#include "char_to_size.hpp"
 #include "complement.hpp"
 #include "global.hpp"
 
 namespace npge {
+
+const int POS_BITS = 2;
+const int BYTE_BITS = 8;
+const hash_t LAST_TWO_BITS = BOOST_BINARY(11);
+
+inline size_t shift_in_hash(int pos) {
+    return ((pos * POS_BITS) % (sizeof(hash_t) * BYTE_BITS));
+}
 
 /** Make hash value from fragment of sequence.
 \param start Beginning of the fragment
 \param length Length of the fragment
 \param ori Orientation of the fragment (1 or -1)
 */
-hash_t make_hash(const char* start, size_t length,
-                 int ori = 1);
+inline hash_t make_hash(const char* start, size_t length,
+                        int ori = 1) {
+    hash_t result = 0;
+    for (int j = 0; j < length; j++) {
+        const char* i = start + j * ori;
+        hash_t value = char_to_size(ori == 1 ?
+                                    *i : complement(*i));
+        result ^= value << shift_in_hash(j);
+    }
+    return result;
+}
 
 /** Make hash value from previous hash value (optimization).
 \param old_hash Previous hash value
@@ -29,9 +50,27 @@ hash_t make_hash(const char* start, size_t length,
 \param add_char New nucleotide, added to the hash value.
 Nucleotides add_char and remove_char should be pre-complement'ed, if needed.
 */
-hash_t reuse_hash(hash_t old_hash, size_t length,
-                  char remove_char, char add_char,
-                  bool forward = true);
+inline hash_t reuse_hash(hash_t old_hash, size_t length,
+                         char remove_char, char add_char,
+                         bool forward = true) {
+    hash_t remove = char_to_size(remove_char);
+    old_hash ^= remove << shift_in_hash(forward ?
+                                        0 : length - 1);
+    int occupied = std::min(POS_BITS * length,
+                            BYTE_BITS * sizeof(hash_t));
+    if (forward) {
+        old_hash = (old_hash >> POS_BITS) |
+                   ((old_hash & LAST_TWO_BITS) <<
+                    (occupied - POS_BITS));
+    } else {
+        old_hash = (old_hash << POS_BITS) |
+                   ((old_hash >> (occupied - POS_BITS)) &
+                    LAST_TWO_BITS);
+    }
+    hash_t add = char_to_size(add_char);
+    old_hash ^= add << shift_in_hash(forward ? length - 1 : 0);
+    return old_hash;
+}
 
 }
 
