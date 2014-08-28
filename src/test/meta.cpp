d6113c2698bfae9c01ec3709b3fae879aa8218b5
Boris Nagaev <bnagaev@gmail.com>
2014-08-28 12:12:32 +0400

    test/meta.cpp: use TssMetaHolder when use own Meta

--- src/test/meta.cpp
+++ src/test/meta.cpp
@@ -9,12 +9,14 @@
 #include <boost/test/unit_test.hpp>
 
 #include "Meta.hpp"
+#include "tss_meta.hpp"
 #include "Processor.hpp"
 
 using namespace npge;
 
 BOOST_AUTO_TEST_CASE (Meta_main) {
     Meta m;
+    TssMetaHolder tmh(&m);
     BOOST_FOREACH (std::string key, m.keys()) {
         BOOST_CHECK(m.has(key));
         BOOST_CHECK(m.get(key)->key() == key);
@@ -23,6 +25,7 @@ BOOST_AUTO_TEST_CASE (Meta_main) {
 
 BOOST_AUTO_TEST_CASE (Meta_options) {
     Meta m;
+    TssMetaHolder tmh(&m);
     m.set_opt("TEST", 33);
     SharedProcessor p = m.get("Processor");
     p->add_gopt("test", "Test", "TEST");
