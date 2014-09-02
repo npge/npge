b1bfd394b2a6644be83aa9716b78c2f0f499223d
Boris Nagaev <bnagaev@gmail.com>
2014-09-02 13:58:23 +0400

    update PostProcessing

--- src/algo/lua_lib.lua
+++ src/algo/lua_lib.lua
@@ -350,115 +350,76 @@ register_p('MergeStemJoin', function()
     return p
 end)
 
+function un_out_info(p, target, base_name)
+    p:add('UniqueNames', 'target=' .. target)
+    p:add('Output', 'target=' .. target ..
+        ' --out-file:=' .. base_name .. '.bs')
+    p:add('BlockInfo', 'target=' .. target ..
+        '--info-count-seqs:=1 '..
+        '--info-file:=' .. base_name .. '.bi')
+end
+
 register_p('PostProcessing', function()
     local p = Pipe.new()
     p:set_name("Postprocess pangenome");
-    p:add('In', '--in-blocks=pangenome0.fasta')
-    p:add('OutputPipe', '--out-file=pangenome.fasta')
+
+    p:add('In', '--in-blocks=pangenome.bs')
+
+    p:add('BlockInfo', '--info-count-seqs:=1 '..
+        '--info-file:=pangenome.bi')
+    p:add('Info', '--out-stats=pangenome.info')
+    p:add('PrintMutations', '--out-file:=pangenome.mutations')
+    p:add('Hash', '--hash-file=pangenome.hash')
+
+    p:add('Union', 'target=stem other=target')
+    p:add('Stem', 'target=stem --exact:=1')
+
+    // check
+
+    p:add('MkDir', '--dirname=check')
 
     p:add('IsPangenome',
-        '--out-is-pangenome=pangenome.isgood '..
-        '--blast-cons-dst=pangenome-cons.fasta '..
-        '--blast-hits-dst=pangenome-hits.blast '..
+        '--out-is-pangenome=check/isgood '..
+        '--blast-cons-dst=check/consensuses.bs '..
+        '--blast-hits-dst=check/hits.blast '..
         'blast-hits=blast-hits joined=joined '..
         'all-blast-hits=all-blast-hits '..
         'non-internal-hits=non-internal-hits')
 
-    // TODO DRY
+    un_out_info(p, 'blast-hits', 'check/good-blast-hits')
+    un_out_info(p, 'all-blast-hits', 'check/all-blast-hits')
+    un_out_info(p, 'non-internal-hits',
+                'check/non-internal-hits')
+    un_out_info(p, 'joined', 'check/joined')
 
-    p:add('UniqueNames', 'target=blast-hits')
-    p:add('Output', 'prefix|blast-good-fasta- '..
-        'target=blast-hits '..
-        '--blast-good-fasta-file=pangenome-blast-good.fasta')
-    p:add('BlockInfo', 'prefix|blast-good-bi- '..
-        'target=blast-hits --blast-good-bi-count-seqs=1 '..
-        '--blast-good-bi-file=pangenome-blast-good.bi')
+    // mutations
 
-    p:add('UniqueNames', 'target=all-blast-hits')
-    p:add('Output', 'prefix|blast-all-fasta- '..
-        'target=all-blast-hits '..
-        '--blast-all-fasta-file=pangenome-blast-all.fasta')
-    p:add('BlockInfo', 'prefix|blast-all-bi- '..
-        'target=all-blast-hits '..
-        '--blast-all-bi-count-seqs=1 '..
-        '--blast-all-bi-file=pangenome-blast-all.bi')
-
-    p:add('UniqueNames', 'target=non-internal-hits')
-    p:add('Output', 'prefix|blast-non-internal-fasta- '..
-        'target=non-internal-hits '..
-        '--blast-non-internal-fasta-file='..
-        'pangenome-blast-non-internal.fasta')
-    p:add('BlockInfo', 'prefix|blast-non-internal-bi- '..
-        '--blast-non-internal-bi-count-seqs=1 '..
-        'target=non-internal-hits '..
-        '--blast-non-internal-bi-file='..
-        'pangenome-blast-non-internal.bi')
-
-    p:add('UniqueNames', 'target=joined')
-    p:add('Output', 'prefix|joined-fasta- target=joined '..
-        '--joined-fasta-file=pangenome-joined.fasta')
-    p:add('BlockInfo', 'prefix|joined-bi- target=joined '..
-        '--joined-bi-count-seqs=1 '..
-        '--joined-bi-file=pangenome-joined.bi')
-
-    p:add('BlockInfo', 'prefix|bi- --bi-count-seqs=1 '..
-        '--bi-file=pangenome.bi')
-    p:add('Info', '--out-stats=pangenome.info')
-    p:add('PrintMutations', 'prefix|mut- '..
-        '--mut-file=pangenome.mutations')
-    p:add('Union', 'target=stem other=target')
-    p:add('Stem', 'target=stem --exact:=1')
     p:add('MutationsSequences', '--mutation-distance=1 '..
         'target=mut other=stem')
-    p:add('Output', 'prefix|mut-fasta- target=mut '..
-        '--mut-fasta-dump-seq:=1 --mut-fasta-dump-block:=0 '..
-        '--mut-fasta-file=pangenome-mutations.fasta')
-    p:add('Output', 'prefix|mut-blocks- target=mut '..
-        '--mut-fasta-dump-seq:=1 --mut-fasta-dump-block:=1 '..
-        '--mut-fasta-file='..
-        'pangenome-mutations-with-blocks.fasta')
-    p:add('PrintTree', '--tree-file=pangenome.trees')
+    p:add('Output', 'target=mut '..
+        '--out-dump-seq:=1 --out-dump-block:=0 '..
+        '--out-file:=mutations/mutseq.fasta')
+    p:add('Output', 'target=mut '..
+        '--out-dump-seq:=1 --out-dump-block:=1 '..
+        '--out-file:=mutations/mutseq-with-blocks.fasta')
+
+    // trees
+
+    p:add('PrintTree', '--tree-file=trees/all_trees.txt')
     p:add('ConsensusTree', 'prefix|nj- --nj-tree-method:=nj '..
-        '--nj-out-consensus-tree=pangenome.constree-nj '..
+        '--nj-out-consensus-tree=trees/nj-constree.tre '..
         '--nj-tree-pseudo-leafs=1 '..
         '--nj-bootstrap-print=before-length')
     p:add('ConsensusTree', 'prefix|upgma- '..
         '--upgma-tree-method:=upgma '..
         '--upgma-out-consensus-tree='..
-        'pangenome.constree-upgma '..
+        'trees/upgma-constree.tre '..
         '--upgma-tree-pseudo-leafs=1 '..
         '--upgma-bootstrap-print=before-length')
     p:add('FragmentDistance',
-        '--distance-file=pangenome.distances')
-    p:add('Hash', '--hash-file=pangenome.hash')
+        '--distance-file=trees/distances.txt')
 
-    p:add('OutputPipe', 'prefix|merged- '..
-        '--merged-out-file=pangenome-merged.fasta')
-    p:add('BlockInfo', 'prefix|merged-bi- '..
-        '--merged-bi-count-seqs=1 '..
-        '--merged-bi-file=pangenome-merged.bi')
-    p:add('Info', 'prefix|merged- '..
-        '--merged-out-stats=pangenome-merged.info')
-    p:add('Hash', 'prefix|merged- '..
-        '--merged-hash-stats=pangenome-merged.hash')
-    p:add('PrintMutations', 'prefix|merged-mut- '..
-        '--merged-mut-file=pangenome-merged.mutations')
-    p:add('Union', 'target=merged-stem other=target')
-    p:add('Stem', 'target=merged-stem --exact:=1')
-    p:add('MutationsSequences', '--mutation-distance=1 '..
-        'target=merged-mut other=merged-stem')
-    p:add('Output', 'prefix|merged-mut-fasta- '..
-        'target=merged-mut '..
-        '--merged-mut-fasta-dump-seq:=1 '..
-        '--merged-mut-fasta-dump-block:=0 '..
-        '--merged-mut-fasta-file='..
-        'pangenome-merged-mutations.fasta')
-    p:add('Output', 'prefix|merged-mut-blocks- '..
-        'target=merged-mut '..
-        '--merged-mut-blocks-dump-seq:=1 '..
-        '--merged-mut-blocks-dump-block:=1 '..
-        '--merged-mut-blocks-file='..
-        'pangenome-merged-mutations-with-blocks.fasta')
+    // BSA
 
     p:add('ChrBSA')
     p:add('PrintBSA', '--out-bsa=pangenome-merged.bsa')
@@ -466,21 +427,15 @@ register_p('PostProcessing', function()
     p:add('PrintBSA', 'prefix|exact-stem- '..
         '--exact-stem-out-bsa=pangenome-merged-exact-stem.bsa')
 
+    // split
+
     p:add('SplitRepeats', 'target=split other=target')
-    p:add('UniqueNames', 'target=split')
-    p:add('Output', 'prefix|split-fasta- target=split '..
-        '--split-fasta-file=pangenome-merged-split.fasta')
-    p:add('BlockInfo', 'prefix|split-bi- target=split '..
-        '--split-bi-count-seqs=1 '..
-        '--split-bi-file=pangenome-merged-split.bi')
+    un_out_info(p, 'split', 'split')
+
+    // low
 
     p:add('FindLowSimilar', 'target=low other=target')
-    p:add('UniqueNames', 'target=low')
-    p:add('Output', 'prefix|low-fasta- target=low '..
-        '--low-fasta-file=pangenome-merged-low.fasta')
-    p:add('BlockInfo', 'prefix|low-bi- target=low '..
-        '--low-bi-count-seqs=1 '..
-        '--low-bi-file=pangenome-merged-low.bi')
+    un_out_info(p, 'low', 'low')
 
     return p
 end)
