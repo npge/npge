04853dbadec41619f058e0e5d46e8c9543952afc
Boris Nagaev <bnagaev@gmail.com>
2014-09-10 11:48:32 +0200

    windows/build.sh: use current source of npge (no download)

--- windows/build.sh
+++ windows/build.sh
@@ -9,10 +9,20 @@ make readline
 make curl
 cd ..
 
-hg clone https://bitbucket.org/starius/npg-explorer
+# get path to dir windows
+script=$0
+if ! (echo $0 | grep -q '/windows') then
+    # maybe the command is "sh .../windows/build.sh"
+    script=$1
+fi
+echo $script
+WINDOWSPATH=`dirname $script`
+
+npge_src=$WINDOWSPATH/../
+
 mkdir -p npge-build
 cd npge-build
 toolchain=$(echo ../mxe/usr/*ming*/share/cmake/mxe-conf.cmake)
-cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain ../npg-explorer/
+cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain $npge_src
 make
 
