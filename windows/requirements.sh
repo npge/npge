d14ed7cdbda3ffbe41d889600dcacb36406bc647
Boris Nagaev <bnagaev@gmail.com>
2014-09-11 10:18:07 +0200

    windows/requirements.sh: add packages needed for packaging

--- windows/requirements.sh
+++ windows/requirements.sh
@@ -1,9 +1,24 @@
 # !Debian: http://mxe.cc/#requirements
 
+# MXE
 sudo apt-get install autoconf automake bash bison bzip2 \
     cmake flex gettext git g++ intltool \
     libffi-dev libtool libltdl-dev libssl-dev \
     libxml-parser-perl make openssl patch perl \
     pkg-config scons sed unzip wget xz-utils \
-    mercurial
+    mercurial \
+    wine-bin:i386 \
+    curl \
+    tar \
+    coreutils \
+    gawk \
+    binutils \
+    upx pandoc zip nsis
+
+# wine-bin to generate AllProcessors.html
+# curl is used to download blast TODO use wget
+# tar is used to unpack blast
+# coreutils for sha1sum
+# gawk to convert Unix line-ends to Windows line-ends
+# binutils for strip
 
