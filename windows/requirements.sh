1da8b2b8b3a13e96312163a4198606a94cf75d1d
Boris Nagaev <bnagaev@gmail.com>
2014-09-13 02:18:48 +0400

    windows/requirements: apt-get build-dep nsis

--- windows/requirements.sh
+++ windows/requirements.sh
@@ -28,6 +28,7 @@ sudo apt-get --yes install autoconf automake bash bison bzip2 \
 # https://community.openvpn.net/openvpn/wiki/%3ABuildingMakeNSIS
 
 sudo apt-get --yes install dpkg-dev devscripts
+sudo apt-get --yes build-dep nsis
 apt-get source nsis
 cd nsis-*
 sed '/^SCONSOPTS_\(NSIS\|COMMON\)/s@SKIPUTILS@NSIS_MAX_STRLEN=8192 SKIPUTILS@' \
