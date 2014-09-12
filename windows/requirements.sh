78218ffbfe33d908bae5f6d7e0bf729d88ed4da8
Boris Nagaev <bnagaev@gmail.com>
2014-09-13 02:06:05 +0400

    debuild -> devscripts

--- windows/requirements.sh
+++ windows/requirements.sh
@@ -25,7 +25,7 @@ sudo apt-get install autoconf automake bash bison bzip2 \
 # Rebuild NSIS with larger NSIS_MAX_STRLEN
 # https://community.openvpn.net/openvpn/wiki/%3ABuildingMakeNSIS
 
-sudo apt-get install dpkg-dev debuild
+sudo apt-get install dpkg-dev devscripts
 apt-get source nsis
 cd nsis-*
 sed '/^SCONSOPTS_\(NSIS\|COMMON\)/s@SKIPUTILS@NSIS_MAX_STRLEN=8192 SKIPUTILS@' \
