effb6f8656713311f6f51ea3bc0a88d0468146e5
Boris Nagaev <bnagaev@gmail.com>
2014-09-13 21:17:36 +0400

    windows/all-steps: log to file npge.log

--- windows/all-steps.sh
+++ windows/all-steps.sh
@@ -1,8 +1,8 @@
-sudo apt-get --yes install mercurial
-hg clone https://bitbucket.org/starius/npg-explorer
+sudo apt-get --yes install mercurial &>> npge.log
+hg clone https://bitbucket.org/starius/npg-explorer &>> npge.log
 cd npg-explorer
-sudo ./windows/requirements.sh
-./windows/build.sh
+sudo ./windows/requirements.sh &>> npge.log
+./windows/build.sh &>> npge.log
 cd npge-build-windows
-./windows/package.sh
+./windows/package.sh &>> npge.log
 
