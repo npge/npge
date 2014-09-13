0bbd2adbd071baf6316e6dc33a79a680a68f1162
Boris Nagaev <bnagaev@gmail.com>
2014-09-13 21:43:18 +0400

    windows/all-steps: fix logging

--- windows/all-steps.sh
+++ windows/all-steps.sh
@@ -1,9 +1,10 @@
-sudo apt-get --yes install mercurial &> npge-1.log
-hg clone https://bitbucket.org/starius/npg-explorer &> npge-2.log
+sudo apt-get --yes install mercurial > npge-1.log 2>&1
+hg clone https://bitbucket.org/starius/npg-explorer \
+    > npge-2.log 2>&1
 cd npg-explorer
 hg pull -u
-sudo ./windows/requirements.sh &> npge-3.log
-./windows/build.sh &> npge-4.log
+sudo ./windows/requirements.sh > npge-3.log 2>&1
+./windows/build.sh > npge-4.log 2>&1
 cd npge-build-windows
-./windows/package.sh &> npge-5.log
+./windows/package.sh > npge-5.log 2>&1
 
