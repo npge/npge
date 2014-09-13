bf0e2a64c0304903a7fe40bc4ddec11cafa68e01
Boris Nagaev <bnagaev@gmail.com>
2014-09-13 20:02:07 +0400

    add script windows/all-steps.sh

---
+++ windows/all-steps.sh
@@ -0,0 +1,8 @@
+sudo apt-get install mercurial
+hg clone https://bitbucket.org/starius/npg-explorer
+cd npg-explorer
+sudo ./windows/requirements.sh
+./windows/build.sh
+cd npge-build-windows
+./windows/package.sh
+
