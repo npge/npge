f714ccf5a232b4895df83909e1b8822e82eaf895
Boris Nagaev <bnagaev@gmail.com>
2014-09-14 13:56:48 +0400

    update linux/requirements
    remove cmake-curses-gui
    replace libboost-all-dev with -dev's of libraries actually used

--- linux/requirements.sh
+++ linux/requirements.sh
@@ -1,6 +1,11 @@
 sudo apt-get --yes install \
-    g++ make cmake cmake-curses-gui \
-    libqt4-dev libboost-all-dev \
+    g++ make cmake \
+    libqt4-dev \
+    liboost-date-time-dev \
+    liboost-program-options-dev \
+    liboost-filesystem-dev \
+    liboost-system-dev \
+    liboost-thread-dev \
     liblua5.1-dev libluabind-dev \
     libreadline-dev libncurses5-dev \
     pandoc doxygen
