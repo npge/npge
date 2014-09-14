3d97e51d97b04e7ec5cbd5cfe50d75ebc99f44f8
Boris Nagaev <bnagaev@gmail.com>
2014-09-14 14:20:48 +0400

    linux/requirements: replace libboost-system-dev
    There are no libboost-system-dev in Debian Lenny

--- linux/requirements.sh
+++ linux/requirements.sh
@@ -1,12 +1,14 @@
 sudo apt-get --yes install \
+    aptitude \
     g++ make cmake \
     libqt4-dev \
     libboost-date-time-dev \
     libboost-program-options-dev \
     libboost-filesystem-dev \
-    libboost-system-dev \
     libboost-thread-dev \
     liblua5.1-dev libluabind-dev \
     libreadline-dev libncurses5-dev \
     pandoc doxygen
+
+sudo aptitude install ~nlibboost-system~ndev
 
