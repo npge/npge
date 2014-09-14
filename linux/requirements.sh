cb6fd289fb417b9393d847d06afac94d16b4062f
Boris Nagaev <bnagaev@gmail.com>
2014-09-14 14:16:06 +0400

    linux/requirements: fix typo

--- linux/requirements.sh
+++ linux/requirements.sh
@@ -1,11 +1,11 @@
 sudo apt-get --yes install \
     g++ make cmake \
     libqt4-dev \
-    liboost-date-time-dev \
-    liboost-program-options-dev \
-    liboost-filesystem-dev \
-    liboost-system-dev \
-    liboost-thread-dev \
+    libboost-date-time-dev \
+    libboost-program-options-dev \
+    libboost-filesystem-dev \
+    libboost-system-dev \
+    libboost-thread-dev \
     liblua5.1-dev libluabind-dev \
     libreadline-dev libncurses5-dev \
     pandoc doxygen
