ce678fa10be072fc5f5a40e64702d270338e413e
Boris Nagaev <bnagaev@gmail.com>
2014-09-14 23:29:57 +0400

    npge_test: set app path
    This helps to find blast in dir of npge_test.exe

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -10,10 +10,20 @@
 
 #include "Meta.hpp"
 #include "throw_assert.hpp"
+#include "name_to_stream.hpp"
 
 namespace npge {
 
 BOOST_GLOBAL_FIXTURE(Meta);
+
+struct ArgsFixture {
+    ArgsFixture() {
+        using namespace boost::unit_test;
+        set_app_path(framework::master_test_suite().argv[0]);
+    }
+};
+
+BOOST_GLOBAL_FIXTURE(ArgsFixture);
 
 }
 
