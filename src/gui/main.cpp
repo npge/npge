e1af8b159742e0f7406631aad586ddb2a8e352ab
Boris Nagaev <bnagaev@gmail.com>
2015-03-14 00:16:48 +0300

    gui: correct HTML (error handling, message window)

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -23,8 +23,10 @@ public:
         } catch (const std::exception& e) {
             using namespace npge;
             QString what = QString::fromStdString(htmlencode(e.what()));
-            QString error = "<b>The error occurred</b>.<br><br>"
-                            "Description for developers:<br><br>" + what;
+            QString error = "<b>The error occurred</b>."
+                            "<br/><br/>"
+                            "Description for developers:"
+                            "<br/><br/>" + what;
             QErrorMessage::qtHandler()->resize(400, 300);
             QErrorMessage::qtHandler()->showMessage(error);
             return false;
