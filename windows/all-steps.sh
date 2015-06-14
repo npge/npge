9c1e5247b82eba5641e9a3e0e67aea4e2e95a2a9
Boris Nagaev <bnagaev@gmail.com>
2015-06-14 19:21:57 +0300

    fix windows x64 build
     * download 64bit blast
     * do not run tests and generation of npge.conf,
        AllProcessors.html and AllOptions.html (wine wont run
        64bit exe files), but copy them from 32bit version
     * name of 64bit version is ...win64...

--- windows/all-steps.sh
+++ windows/all-steps.sh
@@ -7,7 +7,11 @@ git pull
 sudo ./windows/requirements.sh > npge-3.log 2>&1
 ./windows/build.sh > npge-4.log 2>&1
 cd npge-build-windows32
+export BLASTARCH='ia32'
+export NPGEARCH='32'
 ./windows/package.sh > npge-5-32.log 2>&1
 cd ..
 cd npge-build-windows64
+export BLASTARCH='x64'
+export NPGEARCH='64'
 ./windows/package.sh > npge-5-64.log 2>&1
