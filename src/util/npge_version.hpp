2a96e7fd4c360fac89f02de6a0794a8a5b7aa770
Boris Nagaev <bnagaev@gmail.com>
2015-06-28 12:34:28 +0300

    set npge.COMMIT to git commit hash
    see #3

--- src/util/npge_version.hpp
+++ src/util/npge_version.hpp
@@ -12,7 +12,7 @@
 
 namespace npge {
 
-/** Adds global table npge and fields VERSION and ARCH */
+/** Adds global table npge and fields VERSION, ARCH, COMMIT */
 void init_npge_version(lua_State* L);
 
 }
