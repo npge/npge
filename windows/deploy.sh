5b8a5cdfbf93aa0ed90cf8c335452554ae39871b
Boris Nagaev <bnagaev@gmail.com>
2015-10-27 03:37:24 +0300

    Revert "windows: fix using vars NO_WINDOWS.."
    This reverts commit fc2f23af795b14f71564d159f0f9835e4b053486.
    
    This doesn't work.

--- windows/deploy.sh
+++ windows/deploy.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-if [ -z "${NO_WINDOWS32}" ]; then
+if [ -z "$NO_WINDOWS32" ]; then
     cd npge-build-windows32
     export BLASTARCH='ia32'
     export NPGEARCH='32'
@@ -10,7 +10,7 @@ if [ -z "${NO_WINDOWS32}" ]; then
     cd ..
 fi
 
-if [ -z "${NO_WINDOWS64}" ]; then
+if [ -z "$NO_WINDOWS64" ]; then
     cd npge-build-windows64
     export BLASTARCH='x64'
     export NPGEARCH='64'
