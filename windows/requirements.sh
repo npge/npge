db260d4f239a261a152b1eeb4fe7f9ac48f2247f
Boris Nagaev <bnagaev@gmail.com>
2015-10-28 02:03:13 +0300

    use 32bit nsis on win64
    MXE doesn't have 64bit NSIS
    
    See https://travis-ci.org/npge/npge/jobs/87775406#L3155

--- windows/requirements.sh
+++ windows/requirements.sh
@@ -36,4 +36,8 @@ if [ -z "$NOWINDOWS64" ]; then
     sudo apt-get --yes install \
         mxe-x86-64-w64-mingw32.static-{qt,boost,luabind,nsis}
     sudo apt-get clean
+    # MXE doesn't have 64bit NSIS
+    sudo apt-get --yes install \
+        mxe-i686-w64-mingw32.static-nsis
+    sudo apt-get clean
 fi
