075f57bcb68ac144f0065e5bbb60aee9ccd6961e
Boris Nagaev <bnagaev@gmail.com>
2016-01-14 02:03:55 +0300

    fix test bs-read failing on Debian Squeeze
    Add line break after last line.

--- test-lua/bs-read.lua
+++ test-lua/bs-read.lua
@@ -1,6 +1,6 @@
 do
     local bs = BlockSet.new()
-    bs:read(">a\nATGC\n>a_1_1 block=b1 norow")
+    bs:read(">a\nATGC\n>a_1_1 block=b1 norow\n")
     local blocks = bs:iter_blocks()
     local b1 = blocks()
     local f1 = b1:front()
