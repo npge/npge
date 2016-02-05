24124028be6590b219640fd5e52aca975e2f46c6
Boris Nagaev <bnagaev@gmail.com>
2016-02-05 20:14:02 +0300

    embed lua-npge into npge
    fix #77

--- src/util/lua_npge.cpp
+++ src/util/lua_npge.cpp
@@ -5,19 +5,29 @@
  * See the LICENSE file for terms of use.
  */
 
-#include <string>
+#include <iostream>
+
+#include <lua.hpp>
 
 #include "lua_npge.hpp"
-#include "Exception.hpp"
+#include "throw_assert.hpp"
+
+extern "C" {
+int luaopen_npge_cpp(lua_State *L);
+}
 
 namespace npge {
 
 void init_lua_npge(lua_State* L) {
-    std::string code = "npge = require 'npge';"; // FIXME
-    int has_error = luaL_dostring(L, code.c_str());
-    if (has_error) {
-        throw Exception("Can't require module 'npge'.");
-    }
+    // add npge.cpp module
+    lua_getglobal(L, "package");
+    ASSERT_EQ(lua_type(L, -1), LUA_TTABLE);
+    lua_getfield(L, -1, "loaded");
+    luaopen_npge_cpp(L);
+    lua_setfield(L, -2, "npge.cpp"); // package.loaded["npge.cpp"] = ...
+    lua_pop(L, 2); // package and package.loaded
+    // add lua modules of lua-npge
+#include "lua_npge.h"
 }
 
 }
