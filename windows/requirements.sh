8fcc1359fdc4642e9fdc2dc343a03957c43fc4fa
Pavel Dolgov <pdolgov99@gmail.com>
2016-02-08 23:19:33 +0300

    Windows/requirements.sh: default target is win32

--- windows/requirements.sh
+++ windows/requirements.sh
@@ -29,6 +29,9 @@ sudo apt-get --yes install \
 # install MXE packages from http://pkg.mxe.cc/
 
 sudo apt-get clean
+
+MXE_TARGET="${MXE_TARGET-i686-w64-mingw32.static}"
+
 # x86_64 -> x86-64
 MXE2_TARGET=$(echo "$MXE_TARGET" | sed 's/_/-/g')
 sudo apt-get --yes install \
