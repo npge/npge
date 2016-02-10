4d5e9538f3b97a7d7946059a02dc976bd1e172a9
Pavel Dolgov <pdolgov99@gmail.com>
2016-02-10 23:57:41 +0300

    windows/{build,deploy}.sh: set default MXE_TARGET

--- windows/build.sh
+++ windows/build.sh
@@ -16,6 +16,8 @@ npge_src=$WINDOWSPATH/../
 
 MXE_DIR=/usr/lib/mxe
 
+MXE_TARGET="${MXE_TARGET-i686-w64-mingw32.static}"
+
 if [ "$MXE_TARGET" = "i686-w64-mingw32.static" ]; then
     mkdir -p npge-build-windows32
     cd npge-build-windows32
