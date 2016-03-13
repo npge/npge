005e65f61ae4113f6ad636ef894f3ebb333b2797
Boris Nagaev <bnagaev@gmail.com>
2016-03-13 13:28:43 +0300

    init_lua-npge.sh: fix "git clone"
    Checkout to the submodule's directory, not to main directory.

--- src/init_lua-npge.sh
+++ src/init_lua-npge.sh
@@ -8,7 +8,7 @@ COMMIT="388bae6600f47b0806797f90b56e8ae268463645"
 
 lua_npge_dir="src/lua-npge"
 lua_npge_git="$lua_npge_dir/.git"
-git_cmd="git --git-dir=$lua_npge_git"
+git_cmd="git --git-dir=$lua_npge_git --work-tree=$lua_npge_dir"
 
 if [ -e "$lua_npge_git" ]; then
     git submodule update --init
