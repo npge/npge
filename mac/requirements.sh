1c9513207f996a38d98f5b6999a9d9dca3c19c33
Pavel Dolgov <pdolgov99@gmail.com>
2016-07-12 16:22:08 +0300

    mac/requirements.sh: install ncurses via homebrew dupes

--- mac/requirements.sh
+++ mac/requirements.sh
@@ -4,8 +4,10 @@ set -xue
 
 ./src/init_lua-npge.sh
 
+brew install homebrew/dupes/ncurses
+
 brew install \
     qt4 cmake libzip libzzip boost \
-    luajit luabind readline ncurses \
+    luajit luabind readline \
     pandoc tar coreutils binutils
 
