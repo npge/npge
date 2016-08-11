3a98cf976b85a5034267d4a7486be1133606da0e
Boris Nagaev <bnagaev@gmail.com>
2016-08-11 10:59:47 +0300

    fix build on OSX (unlink && install)
    brew fails if a package is already installed:
    https://travis-ci.org/npge/npge/jobs/151420671#L396
    See also https://docs.travis-ci.com/user/osx-ci-environment/#A-note-on-upgrading-packages

--- mac/requirements.sh
+++ mac/requirements.sh
@@ -7,9 +7,16 @@ set -xue
 brew update
 brew install homebrew/dupes/ncurses
 
-brew install \
+for pkg in \
     qt4 cmake libzip libzzip \
     boost lua51 luabind \
-    pandoc gnu-tar coreutils binutils
+    pandoc gnu-tar coreutils binutils \
+; do
+    # brew fails if a package is already installed:
+    # https://travis-ci.org/npge/npge/jobs/151420671#L396
+    # See also https://docs.travis-ci.com/user/osx-ci-environment/#A-note-on-upgrading-packages
+    brew unlink $pkg || true
+    brew install $pkg
+done
 
 ln -s /usr/local/bin/gtac /usr/local/bin/tac
