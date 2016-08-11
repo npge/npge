9f32c24afa9f8958040fd584d15d2c193a0dd509
Boris Nagaev <bnagaev@gmail.com>
2016-08-11 09:18:05 +0300

    test with MXE APT with sha256

--- windows/requirements.sh
+++ windows/requirements.sh
@@ -5,7 +5,7 @@ set -xue
 ./src/init_lua-npge.sh
 
 # pre-built MXE packages
-echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" \
+echo "deb http://pkg.mxe.cc/releases/2015-12-16-sha256/repos/apt/debian wheezy main" \
     | sudo tee /etc/apt/sources.list.d/mxeapt.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com \
     --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
